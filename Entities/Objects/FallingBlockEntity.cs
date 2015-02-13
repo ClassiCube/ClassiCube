c93605f1fcb3781ebdf59c10ed45b09a714ef7be
UnknownShadow200 <unknownshadow200@gmail.com>
2015-02-14 08:45:34 +1100

    Move object entities into their own folder.

---
+++ Entities/Objects/FallingBlockEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OpenTK;
+
+namespace ClassicalSharp {
+
+	public sealed class FallingBlockEntity : ObjectEntity {		
+		
+		public override float StepSize {
+			get { return 0; }
+		}
+		
+		public override OpenTK.Vector3 Size {
+			get { return Vector3.One; }
+		}
+		
+		public FallingBlockEntity( ObjectEntityType type, int data, Game window ) : base( type, data, window ) {
+		}
+	}
+}
