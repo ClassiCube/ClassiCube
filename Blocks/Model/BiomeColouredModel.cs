65356ad71b1399c100ab7fbae39a07aaa7f82a4c
UnknownShadow200 <unknownshadow200@gmail.com>
2015-02-21 07:02:16 +1100

    Blending for grass/foliage models.

--- Blocks/Model/BiomeColouredModel.cs
+++ Blocks/Model/BiomeColouredModel.cs
@@ -25,12 +25,25 @@ namespace ClassicalSharp.Blocks.Model {
 		float[] scales = new float[] { 0.6f, 0.6f, 0.8f, 0.8f, 0.5f, 1f };
 		public override void DrawFace( int face, byte meta, ref Neighbours state, ref int index, float x, float y, float z,
 		                              VertexPos3fTex2fCol4b[] vertices, FastColour col ) {
-			// TODO: Also according to the page, we have to average the eight neighbours for proper biome blending.
-			// Though since we don't even use biomes yet, I guess that doesn't really matter.
-			int colour = info.GetColour( (float)( x % 64 ) / 63, (float)( z % 64 ) / 63 );
-			col = new FastColour( ( colour & 0xFF0000 ) >> 16, ( colour & 0xFF00 ) >> 8, ( colour & 0xFF ) );
+			//int colour = info.GetColour( (float)( x % 64 ) / 63, (float)( z % 64 ) / 63 );
+			//col = new FastColour( ( colour & 0xFF0000 ) >> 16, ( colour & 0xFF00 ) >> 8, ( colour & 0xFF ) );
+			col = BlendedColour( x, y, z );
 			col = FastColour.Scale( col, scales[face] );			
 			drawer.DrawFace( face, meta, ref state, ref index, x, y, z, vertices, col );
+		}
+		
+		FastColour BlendedColour( float x, float y, float z ) {
+			int sumR = 0, sumG = 0, sumB = 0;
+			for( int xx = - 1; xx <= 1; xx++ ) {
+				for( int zz = -1; zz <= 1; zz++ ) {
+					int col = info.GetColour( (float)( ( x + xx ) % 64 ) / 63, 
+					                      (float)( ( z + zz ) % 64 ) / 63 );
+					sumR += ( col & 0xFF0000 ) >> 16;
+					sumG += ( col & 0xFF00 ) >> 8;
+					sumB += ( col & 0xFF );
+				}
+			}
+			return new FastColour( sumR / 9, sumG / 9, sumB / 9 );
 		}
 	}
 }
--- Blocks/Model/GrassCubeModel.cs
+++ Blocks/Model/GrassCubeModel.cs
@@ -11,10 +11,25 @@ namespace ClassicalSharp.Blocks.Model {
 		public override void DrawFace( int face, byte meta, ref Neighbours state, ref int index, float x, float y, float z,
 		                              VertexPos3fTex2fCol4b[] vertices, FastColour col ) {
 			if( face == TileSide.Top ) {
-				int colour = info.GetColour( (float)( x % 64 ) / 63, (float)( z % 64 ) / 63 );
-				col = new FastColour( ( colour & 0xFF0000 ) >> 16, ( colour & 0xFF00 ) >> 8, ( colour & 0xFF ) );
+				//int colour = info.GetColour( (float)( x % 64 ) / 63, (float)( z % 64 ) / 63 );
+				//col = new FastColour( ( colour & 0xFF0000 ) >> 16, ( colour & 0xFF00 ) >> 8, ( colour & 0xFF ) );
+				col = BlendedColour( x, y, z );
 			}
 			base.DrawFace( face, meta, ref state, ref index, x, y, z, vertices, col );
+		}
+		
+		FastColour BlendedColour( float x, float y, float z ) {
+			int sumR = 0, sumG = 0, sumB = 0;
+			for( int xx = - 1; xx <= 1; xx++ ) {
+				for( int zz = -1; zz <= 1; zz++ ) {
+					int col = info.GetColour( (float)( ( x + xx ) % 64 ) / 63, 
+					                      (float)( ( z + zz ) % 64 ) / 63 );
+					sumR += ( col & 0xFF0000 ) >> 16;
+					sumG += ( col & 0xFF00 ) >> 8;
+					sumB += ( col & 0xFF );
+				}
+			}
+			return new FastColour( sumR / 9, sumG / 9, sumB / 9 );
 		}
 	}
 }
