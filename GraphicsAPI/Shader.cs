ff72bab9460b0570d3f9edd92fe542881a2d32df
UnknownShadow200 <unknownshadow200@gmail.com>
2015-06-19 19:14:13 +1000

    Add base class for shaders.

---
+++ GraphicsAPI/Shader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ClassicalSharp.GraphicsAPI {
+
+	public abstract class Shader {
+		
+		public string VertexSource;
+		
+		public string FragmentSource;
+		
+		public int ProgramId;
+		
+		public void Init( OpenGLApi api ) {
+			ProgramId = api.CreateProgram( VertexSource, FragmentSource );
+			GetLocations( api );
+		}
+		
+		public void Draw( OpenGLApi api, DrawMode mode, VertexFormat format, int id, int startVertex, int verticesCount ) {
+			EnableVertexAttribStates( api );
+			api.DrawVb( mode, format, id, startVertex, verticesCount );
+			DisableVertexAttribStates( api );
+		}
+		
+		public void DrawIndexedVbBatch( OpenGLApi api, DrawMode mode, int vb, int ib, int indicesCount,
+		                               int startVertex, int startIndex ) {
+			EnableVertexAttribStates( api );
+			api.DrawIndexedVbBatch( mode, vb, ib, indicesCount, startVertex, startIndex );
+			DisableVertexAttribStates( api );
+		}
+		
+		protected virtual void GetLocations( OpenGLApi api ) {		
+		}
+		
+		protected virtual void EnableVertexAttribStates( OpenGLApi api ) {			
+		}
+		
+		protected virtual void DisableVertexAttribStates( OpenGLApi api ) {
+		}
+	}
+}
