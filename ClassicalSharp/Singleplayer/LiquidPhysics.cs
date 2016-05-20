e426f7859b5dba89cb7dd722380268df8f96b623
UnknownShadow200 <unknownshadow200@gmail.com>
2016-05-20 16:31:50 +1000

    Modularise out liquid physics.

---
+++ ClassicalSharp/Singleplayer/LiquidPhysics.cs
@@ -0,0 +1,146 @@
+﻿// ClassicalSharp copyright 2014-2016 UnknownShadow200 | Licensed under MIT
+using System;
+using System.Collections.Generic;
+using ClassicalSharp.Map;
+using OpenTK;
+
+namespace ClassicalSharp.Singleplayer {
+
+	public class LiquidPhysics {
+		
+		Game game;
+		World map;
+		Random rnd = new Random();
+		BlockInfo info;
+		int width, length, height, oneY;
+		int maxX, maxY, maxZ, maxWaterX, maxWaterY, maxWaterZ;
+
+		public LiquidPhysics( Game game ) {
+			this.game = game;
+			map = game.World;
+			info = game.BlockInfo;
+		}
+		
+		public void Clear() { Lava.Clear(); Water.Clear(); }
+		
+		public void ResetMap() {
+			Clear();
+			width = map.Width; maxX = width - 1; maxWaterX = maxX - 2;
+			height = map.Height; maxY = height - 1; maxWaterY = maxY - 2;
+			length = map.Length; maxZ = length - 1; maxWaterZ = maxZ - 2;			
+			oneY = width * length;
+		}
+
+		
+		Queue<uint> Lava = new Queue<uint>();
+		const uint defLavaTick = 30u << Physics.tickShift;
+		
+		public void PlaceLava( int index ) {
+			Lava.Enqueue( defLavaTick | (uint)index );
+		}
+		
+		public void TickLava() {
+			int count = Lava.Count;
+			for( int i = 0; i < count; i++ ) {
+				int index;
+				if( Physics.CheckItem( Lava, out index ) ) {
+					byte block = map.blocks[index];
+					if( !(block == (byte)Block.Lava || block == (byte)Block.StillLava) ) continue;
+					HandleLava( index );
+				}
+			}
+		}
+		
+		public void HandleLava( int index ) {
+			int x = index % width;
+			int y = index / oneY; // posIndex / (width * length)
+			int z = (index / width) % length;
+			
+			if( x > 0 ) PropagateLava( index - 1, x - 1, y, z );
+			if( x < width - 1 ) PropagateLava( index + 1, x + 1, y, z );
+			if( z > 0 ) PropagateLava( index - width, x, y, z - 1 );
+			if( z < length - 1 ) PropagateLava( index + width, x, y, z + 1 );
+			if( y > 0 ) PropagateLava( index - oneY, x, y - 1, z );
+		}
+		
+		void PropagateLava( int posIndex, int x, int y, int z ) {
+			byte block = map.blocks[posIndex];
+			if( block == (byte)Block.Water || block == (byte)Block.StillWater ) {
+				game.UpdateBlock( x, y, z, (byte)Block.Stone );
+			} else if( info.Collide[block] == CollideType.WalkThrough ) {
+				Lava.Enqueue( defLavaTick | (uint)posIndex );
+				game.UpdateBlock( x, y, z, (byte)Block.Lava );
+			}
+		}
+		
+		Queue<uint> Water = new Queue<uint>();
+		const uint defWaterTick = 5u << Physics.tickShift;
+		
+		public void PlaceWater( int index ) {
+			Water.Enqueue( defWaterTick | (uint)index );
+		}
+		
+		public void TickWater() {
+			int count = Water.Count;
+			for( int i = 0; i < count; i++ ) {
+				int index;
+				if( Physics.CheckItem( Water, out index ) ) {
+					byte block = map.blocks[index];
+					if( !(block == (byte)Block.Water || block == (byte)Block.StillWater) ) continue;
+					HandleWater( index );
+				}
+			}
+		}
+		
+		public void HandleWater( int index ) {
+			int x = index % width;
+			int y = index / oneY; // posIndex / (width * length)
+			int z = (index / width) % length;
+			
+			if( x > 0 ) PropagateWater( index - 1, x - 1, y, z );
+			if( x < width - 1 ) PropagateWater( index + 1, x + 1, y, z );
+			if( z > 0 ) PropagateWater( index - width, x, y, z - 1 );
+			if( z < length - 1 ) PropagateWater( index + width, x, y, z + 1 );
+			if( y > 0 ) PropagateWater( index - oneY, x, y - 1, z );
+		}
+		
+		void PropagateWater( int posIndex, int x, int y, int z ) {
+			byte block = map.blocks[posIndex];
+			if( block == (byte)Block.Lava || block == (byte)Block.StillLava ) {
+				game.UpdateBlock( x, y, z, (byte)Block.Stone );
+			} else if( info.Collide[block] == CollideType.WalkThrough && block != (byte)Block.Rope ) {
+				if( CheckIfSponge( x, y, z ) ) return;
+				Water.Enqueue( defWaterTick | (uint)posIndex );
+				game.UpdateBlock( x, y, z, (byte)Block.Water );
+			}
+		}
+
+		
+		public void PlaceSponge( int index ) {
+			int x = index % width;
+			int y = index / oneY; // posIndex / (width * length)
+			int z = (index / width) % length;
+					
+			for( int yy = y - 2; yy <= y + 2; yy++ )
+				for( int zz = z - 2; zz <= z + 2; zz++ )
+					for( int xx = x - 2; xx <= x + 2; xx++ )
+			{
+				byte block = map.SafeGetBlock( xx, yy, zz );
+				if( block == (byte)Block.Water || block == (byte)Block.StillWater )
+					game.UpdateBlock( xx, yy, zz, (byte)Block.Air );
+			}
+		}
+		
+		bool CheckIfSponge( int x, int y, int z ) {
+			for( int yy = (y < 2 ? 0 : y - 2); yy <= (y > maxWaterY ? maxY : y + 2); yy++ )
+				for( int zz = (z < 2 ? 0 : z - 2); zz <= (z > maxWaterZ ? maxZ : z + 2); zz++ )
+					for( int xx = (x < 2 ? 0 : x - 2); xx <= (x > maxWaterX ? maxX : x + 2); xx++ )
+			{
+				byte block = map.blocks[(yy * length + zz) * width + xx];
+				if( block == (byte)Block.Sponge ) return true;
+			}
+			
+			return false;
+		}
+	}
+}
--- ClassicalSharp/Singleplayer/Physics.cs
+++ ClassicalSharp/Singleplayer/Physics.cs
@@ -21,6 +21,7 @@ namespace ClassicalSharp.Singleplayer {
 		FallingPhysics falling;
 		TNTPhysics tnt;
 		FoilagePhysics foilage;
+		LiquidPhysics liquid;
 		
 		bool enabled = true;
 		public bool Enabled {
@@ -34,9 +35,11 @@ namespace ClassicalSharp.Singleplayer {
 			info = game.BlockInfo;
 			game.WorldEvents.OnNewMapLoaded += ResetMap;
 			enabled = Options.GetBool( OptionsKey.SingleplayerPhysics, true );
+			
 			falling = new FallingPhysics( game );
 			tnt = new TNTPhysics( game );
 			foilage = new FoilagePhysics( game );
+			liquid = new LiquidPhysics( game );
 		}
 		
 		internal static bool CheckItem( Queue<uint> queue, out int posIndex ) {
@@ -73,8 +76,8 @@ namespace ClassicalSharp.Singleplayer {
 			if( !Enabled || game.World.IsNotLoaded ) return;
 			
 			//if( (tickCount % 5) == 0 ) {
-			TickLava();
-			TickWater();
+			liquid.TickLava();
+			liquid.TickWater();
 			falling.Tick();
 			//}
 			tickCount++;
@@ -86,9 +89,9 @@ namespace ClassicalSharp.Singleplayer {
 			
 			int index = (y * length + z) * width + x;
 			if( block == (byte)Block.Lava ) {
-				Lava.Enqueue( defLavaTick | (uint)index );
+				liquid.PlaceLava( index );
 			} else if( block == (byte)Block.Water ) {
-				Water.Enqueue( defWaterTick | (uint)index );
+				liquid.PlaceWater( index );
 			} else if( block == (byte)Block.Sand || block == (byte)Block.Gravel ) {
 				falling.OnBlockPlace( index, block );
 			} else if( block == (byte)Block.TNT ) {
@@ -96,12 +99,13 @@ namespace ClassicalSharp.Singleplayer {
 			} else if( block == (byte)Block.Sapling ) {
 				foilage.GrowTree( x, y, z );
 			} else if( block == (byte)Block.Sponge ) {
-				PlaceSponge( x, y, z );
+				liquid.PlaceSponge( index );
 			}
 		}
 		
 		void ResetMap( object sender, EventArgs e ) {
 			falling.ResetMap();
+			liquid.ResetMap();
 			width = map.Width; maxX = width - 1; maxWaterX = maxX - 2;
 			height = map.Height; maxY = height - 1; maxWaterY = maxY - 2;
 			length = map.Length; maxZ = length - 1; maxWaterZ = maxZ - 2;			
@@ -109,16 +113,14 @@ namespace ClassicalSharp.Singleplayer {
 		}
 		
 		void ClearQueuedEvents() {
-			Lava.Clear();
-			Water.Clear();
+			liquid.Clear();
 			falling.Clear();
 		}
 		
 		public void Dispose() {
 			game.WorldEvents.OnNewMapLoaded -= ResetMap;
 		}
-		
-		#region General
+
 		
 		void TickRandomBlocks() {
 			int xMax = width - 1, yMax = height - 1, zMax = length - 1;
@@ -149,9 +151,8 @@ namespace ClassicalSharp.Singleplayer {
 					break;
 					
 				case (byte)Block.Grass:
-					if( y <= map.GetLightHeight( x, z ) ) {
+					if( y <= map.GetLightHeight( x, z ) )
 						game.UpdateBlock( x, y, z, (byte)Block.Dirt );
-					}
 					break;
 					
 				case (byte)Block.Dandelion:
@@ -172,123 +173,13 @@ namespace ClassicalSharp.Singleplayer {
 					break;
 					
 				case (byte)Block.Lava:
-					HandleLava( posIndex, x, y, z );
+					liquid.HandleLava( posIndex );
 					break;
 					
 				case (byte)Block.Water:
-					HandleWater( posIndex, x, y, z );
+					liquid.HandleWater( posIndex );
 					break;
 			}
 		}
-		#endregion
-		
-		#region Lava
-		
-		Queue<uint> Lava = new Queue<uint>();
-		const uint defLavaTick = 30u << tickShift;
-		
-		void TickLava() {
-			int count = Lava.Count;
-			for( int i = 0; i < count; i++ ) {
-				int posIndex;
-				if( CheckItem( Lava, out posIndex ) ) {
-					byte block = map.blocks[posIndex];
-					if( !(block == (byte)Block.Lava || block == (byte)Block.StillLava) ) continue;
-					
-					int x = posIndex % width;
-					int y = posIndex / oneY; // posIndex / (width * length)
-					int z = (posIndex / width) % length;
-					HandleLava( posIndex, x, y, z );
-				}
-			}
-		}
-		
-		void HandleLava( int posIndex, int x, int y, int z ) {
-			if( x > 0 ) PropagateLava( posIndex - 1, x - 1, y, z );
-			if( x < width - 1 ) PropagateLava( posIndex + 1, x + 1, y, z );
-			if( z > 0 ) PropagateLava( posIndex - width, x, y, z - 1 );
-			if( z < length - 1 ) PropagateLava( posIndex + width, x, y, z + 1 );
-			if( y > 0 ) PropagateLava( posIndex - oneY, x, y - 1, z );
-		}
-		
-		void PropagateLava( int posIndex, int x, int y, int z ) {
-			byte block = map.blocks[posIndex];
-			if( block == (byte)Block.Water || block == (byte)Block.StillWater ) {
-				game.UpdateBlock( x, y, z, (byte)Block.Stone );
-			} else if( info.Collide[block] == CollideType.WalkThrough ) {
-				Lava.Enqueue( defLavaTick | (uint)posIndex );
-				game.UpdateBlock( x, y, z, (byte)Block.Lava );
-			}
-		}
-		
-		#endregion
-		
-		#region Water
-		
-		Queue<uint> Water = new Queue<uint>();
-		const uint defWaterTick = 5u << tickShift;
-		
-		void TickWater() {
-			int count = Water.Count;
-			for( int i = 0; i < count; i++ ) {
-				int posIndex;
-				if( CheckItem( Water, out posIndex ) ) {
-					byte block = map.blocks[posIndex];
-					if( !(block == (byte)Block.Water || block == (byte)Block.StillWater) ) continue;
-					
-					int x = posIndex % width;
-					int y = posIndex / oneY; // posIndex / (width * length)
-					int z = (posIndex / width) % length;
-					HandleWater( posIndex, x, y, z );
-				}
-			}
-		}
-		
-		void HandleWater( int posIndex, int x, int y, int z ) {
-			if( x > 0 ) PropagateWater( posIndex - 1, x - 1, y, z );
-			if( x < width - 1 ) PropagateWater( posIndex + 1, x + 1, y, z );
-			if( z > 0 ) PropagateWater( posIndex - width, x, y, z - 1 );
-			if( z < length - 1 ) PropagateWater( posIndex + width, x, y, z + 1 );
-			if( y > 0 ) PropagateWater( posIndex - oneY, x, y - 1, z );
-		}
-		
-		void PropagateWater( int posIndex, int x, int y, int z ) {
-			byte block = map.blocks[posIndex];
-			if( block == (byte)Block.Lava || block == (byte)Block.StillLava ) {
-				game.UpdateBlock( x, y, z, (byte)Block.Stone );
-			} else if( info.Collide[block] == CollideType.WalkThrough && block != (byte)Block.Rope ) {
-				if( CheckIfSponge( x, y, z ) ) return;
-				Water.Enqueue( defWaterTick | (uint)posIndex );
-				game.UpdateBlock( x, y, z, (byte)Block.Water );
-			}
-		}
-		
-		#endregion
-		
-		#region Sponge
-		
-		void PlaceSponge( int x, int y, int z ) {
-			for( int yy = y - 2; yy <= y + 2; yy++ )
-				for( int zz = z - 2; zz <= z + 2; zz++ )
-					for( int xx = x - 2; xx <= x + 2; xx++ )
-			{
-				byte block = map.SafeGetBlock( xx, yy, zz );
-				if( block == (byte)Block.Water || block == (byte)Block.StillWater )
-					game.UpdateBlock( xx, yy, zz, (byte)Block.Air );
-			}
-		}
-		
-		bool CheckIfSponge( int x, int y, int z ) {
-			for( int yy = (y < 2 ? 0 : y - 2); yy <= (y > maxWaterY ? maxY : y + 2); yy++ )
-				for( int zz = (z < 2 ? 0 : z - 2); zz <= (z > maxWaterZ ? maxZ : z + 2); zz++ )
-					for( int xx = (x < 2 ? 0 : x - 2); xx <= (x > maxWaterX ? maxX : x + 2); xx++ )
-			{
-				byte block = map.blocks[(yy * length + zz) * width + xx];
-				if( block == (byte)Block.Sponge ) return true;
-			}
-			
-			return false;
-		}
-		#endregion
 	}
 }
