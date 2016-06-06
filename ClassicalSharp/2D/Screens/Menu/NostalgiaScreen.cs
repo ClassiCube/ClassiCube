c738c22e32677a170846031a1a5355f33363822a
UnknownShadow200 <unknownshadow200@gmail.com>
2016-06-06 22:06:43 +1000

    Fix tooltip in nostalgia options not being resized. (Thanks 123DMWM)

--- ClassicalSharp/2D/Screens/Menu/NostalgiaScreen.cs
+++ ClassicalSharp/2D/Screens/Menu/NostalgiaScreen.cs
@@ -64,6 +64,11 @@ namespace ClassicalSharp.Gui {
 			api.Texturing = false;
 		}
 		
+		public override void OnResize( int oldWidth, int oldHeight, int width, int height ) {
+			base.OnResize( oldWidth, oldHeight, width, height );
+			infoWidget.OnResize( oldWidth, oldHeight, width, height );
+		}
+		
 		public override void Dispose() {
 			base.Dispose();
 			infoWidget.Dispose();
