92c9f2e8eb86d1d89b87344c05054102a5661d0f
UnknownShadow200 <unknownshadow200@gmail.com>
2016-10-18 11:13:49 +1100

    Release 0.99.4

--- ClassicalSharp/Program.cs
+++ ClassicalSharp/Program.cs
@@ -9,7 +9,7 @@ namespace ClassicalSharp {
 	
 	internal static class Program {
 		
-		public const string AppName = "ClassicalSharp 0.99.3";
+		public const string AppName = "ClassicalSharp 0.99.4";
 		
 		public static string AppDirectory;
 		
