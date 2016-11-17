7b8c9b4e56f014aa5acf690d1d2271c89629bf1e
UnknownShadow200 <unknownshadow200@gmail.com>
2016-11-17 22:58:07 +1100

    Style: move stuff out of FixedBufferStream and reuse it for Ionic.Zlib

---
+++ ClassicalSharp/Utils/ReadOnlyStream.cs
@@ -0,0 +1,28 @@
+﻿// ClassicalSharp copyright 2014-2016 UnknownShadow200 | Licensed under MIT
+using System;
+using System.IO;
+
+namespace ClassicalSharp {
+	
+	/// <summary> Implements a non-seekable stream that can only be read from. </summary>
+	internal abstract class ReadOnlyStream : Stream {
+		
+		static NotSupportedException ex = new NotSupportedException("Writing/Seeking not supported");
+		
+		public override bool CanRead { get { return true; } }
+		
+		public override bool CanSeek { get { return false; } }
+		
+		public override bool CanWrite { get { return false; } }
+		
+		public override long Length { get { throw ex; } }
+		
+		public override long Position { get { throw ex; } set { throw ex; } }
+		
+		public override long Seek( long offset, SeekOrigin origin ) { throw ex; }
+		
+		public override void SetLength( long value ) { throw ex; }
+		
+		public override void Write( byte[] buffer, int offset, int count ) { throw ex; }
+	}
+}
