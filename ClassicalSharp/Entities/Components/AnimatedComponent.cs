95125b2bf78c82f6d9ddfdc8ae08ea1560584544
Goodlyay <zackxv7@yahoo.com>
2017-03-04 22:43:54 -0800

    Fix view bobbing reduction speed

--- ClassicalSharp/Entities/Components/AnimatedComponent.cs
+++ ClassicalSharp/Entities/Components/AnimatedComponent.cs
@@ -76,7 +76,7 @@ namespace ClassicalSharp.Entities {
 		}
 		
 		static void DoTilt(ref float tilt, bool reduce) {
-			if (reduce) tilt *= 0.93f;
+			if (reduce) tilt *= 0.84f;
 			else tilt += 0.1f;
 			Utils.Clamp(ref tilt, 0, 1);
 		}
