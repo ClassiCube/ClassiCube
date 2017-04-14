10a0364a8983a4f14e735908b8951cd11186adb1
UnknownShadow200 <unknownshadow200@gmail.com>
2017-04-14 18:14:52 +1000

    oops

--- ClassicalSharp/2D/Screens/Inventory/InventoryScreen.cs
+++ ClassicalSharp/2D/Screens/Inventory/InventoryScreen.cs
@@ -222,7 +222,7 @@ namespace ClassicalSharp.Gui.Screens {
 			}
 		}
 		
-		bool Show(BlockID block) { return true;
+		bool Show(BlockID block) {
 			if (game.PureClassic && IsHackBlock(block)) return false;
 			if (block < Block.CpeCount) {
 				int count = game.UseCPEBlocks ? Block.CpeCount : Block.OriginalCount;
