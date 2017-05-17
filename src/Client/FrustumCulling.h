ad760bcc9644df15df3cfdca64585e8deb4e70b4
UnknownShadow200 <unknownshadow200@gmail.com>
2017-05-17 21:52:26 +1000

    Port frustum culling to C

---
+++ src/Client/FrustumCulling.h
@@ -0,0 +1,17 @@
+#ifndef CS_FRUSTUMCULLING_H
+#define CS_FRUSTUMCULLING_H
+#include "Matrix.h"
+#include "Typedefs.h"
+/* Implements frustum culling of bounding spheres.
+   Copyright 2014-2017 ClassicalSharp | Licensed under BSD-3
+    Sourced from http://www.crownandcutlass.com/features/technicaldetails/frustum.html
+*/
+
+/* Returns whether the given bounding sphere is in the view frustum. */
+bool FrustumCulling_SphereInFrustum(Real32 x, Real32 y, Real32 z, Real32 radius);
+
+/* Calculates the planes that constitute view frustum. */
+void FrustumCulling_CalcFrustumEquations(Matrix* projection, Matrix* modelView);
+
+static void FrustumCulling_Normalise(Real32* plane0, Real32* plane1, Real32* plane2, Real32* plane3);
+#endif
