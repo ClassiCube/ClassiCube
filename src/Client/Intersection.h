0d17e113b260a89fb7561229be43548299886901
UnknownShadow200 <unknownshadow200@gmail.com>
2017-05-18 23:18:54 +1000

    Port IntersectionUtils to C

---
+++ src/Client/Intersection.h
@@ -0,0 +1,18 @@
+#ifndef CS_INTERSECTION_H
+#define CS_INTERSECTION_H
+#include "Typedefs.h"
+#include "Vectors.h"
+/* Contains various methods for intersecting geometry.
+   Copyright 2014-2017 ClassicalSharp | Licensed under BSD-3
+*/
+
+
+/* Calculates the intersection points of a ray and a rotated bounding box. */
+bool Intersection_RayIntersectsRotatedBox(Vector3 origin, Vector3 dir, Entity* target, Real32* tMin, Real32* tMax);
+
+static Vector3 Intersection_InverseRotate(Vector3 pos, Entity* target);
+
+/* Calculates the intersection points of a ray and a bounding box.
+Source: http://www.cs.utah.edu/~awilliam/box/box.pdf */
+bool Intersection_RayIntersectsBox(Vector3 origin, Vector3 dir, Vector3 min, Vector3 max, Real32* t0, Real32* t1);
+#endif
