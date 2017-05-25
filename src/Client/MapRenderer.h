0beb681c1af67cf315d532b5405b3a00f448e2f4
UnknownShadow200 <unknownshadow200@gmail.com>
2017-05-25 14:56:08 +1000

    Partially port MapRenderer to C.

---
+++ src/Client/MapRenderer.h
@@ -0,0 +1,84 @@
+#ifndef CS_MAPRENDERER_H
+#define CS_MAPRENDERER_H
+#include "Typedefs.h"
+#include "TerrainAtlas1D.h"
+#include "ChunkInfo.h"
+/* Renders the blocks of the world by subdividing it into chunks.
+Copyright 2014-2017 ClassicalSharp | Licensed under BSD-3
+*/
+
+/* Width of the world, in terms of number of chunks. */
+Int32 MapRenderer_ChunksX;
+
+/* Height of the world, in terms of number of chunks. */
+Int32 MapRenderer_ChunksY;
+
+/* Length of the world, in terms of number of chunks. */
+Int32 MapRenderer_ChunksZ;
+
+/* Packs a coordinate into a single integer index. */
+#define MapRenderer_Pack(cx, cy, cz) (((cy) * MapRenderer_ChunksZ + (cz)) * MapRenderer_ChunksX + (cx))
+
+
+/* The count of actual used 1D atlases. (i.e. 1DIndex(maxTextureLoc) + 1*/
+Int32 MapRenderer_1DUsedCount;
+
+/* The number of non-empty ChunkPartInfos (across entire world) for each 1D atlas batch.
+1D atlas batches that do not have any ChunkPartInfos can be entirely skipped. */
+Int32 MapRenderer_PartsCount[Atlas1D_MaxAtlasesCount];
+
+/* Whether there are any visible Translucent ChunkPartInfos for each 1D atlas batch.
+1D atlas batches that do not have any visible translucent ChunkPartInfos can be skipped. */
+bool MapRenderer_HasTranslucentParts[Atlas1D_MaxAtlasesCount];
+
+/* Whether there are any visible Normal ChunkPartInfos for each 1D atlas batch.
+1D atlas batches that do not have any visible normal ChunkPartInfos can be skipped. */
+bool MapRenderer_HasNormalParts[Atlas1D_MaxAtlasesCount];
+
+/* Whether renderer should check if there are any visible Translucent ChunkPartInfos for each 1D atlas batch. */
+bool MapRenderer_CheckingHasTranslucentParts[Atlas1D_MaxAtlasesCount];
+
+/* Whether renderer should check if there are any visible Normal ChunkPartInfos for each 1D atlas batch. */
+bool MapRenderer_CheckingHasNormalParts[Atlas1D_MaxAtlasesCount];
+
+
+/* Render info for all chunks in the world. Unsorted.*/
+ChunkInfo* MapRenderer_Chunks;
+
+/* The number of chunks in the world, or ChunksX * ChunksY * ChunksZ */
+Int32 MapRenderer_ChunksCount;
+
+/* Pointers to render info for all chunks in the world, sorted by distance from the camera. */
+ChunkInfo** MapRenderer_SortedChunks;
+
+/* Pointers to render info for all chunks in the world, sorted by distance from the camera.
+Chunks that can be rendered (not empty and are visible) are included in this array. */
+ChunkInfo** MapRenderer_RendererChunks;
+
+/* The number of actually used pointers in the RenderChunks array.
+Entries past this count should be ignored and skipped. */
+Int32 MapRenderer_RenderChunksCount;
+
+/* Retrieves the render info for the given chunk. */
+ChunkInfo* MapRenderer_GetChunk(Int32 cx, Int32 cy, Int32 cz);
+
+/* Marks the given chunk as needing to be deleted. */
+void MapRenderer_RefreshChunk(Int32 cx, Int32 cy, Int32 cz);
+
+/* Potentially generates meshes for several pending chunks. */
+void MapRenderer_Update(Real64 deltaTime);
+
+/* Renders all opaque and transparent blocks.
+Pixels are either treated as fully replacing existing pixel, or skipped. */
+void MapRenderer_RenderNormal(Real64 deltaTime);
+
+/*Renders all translucent (e.g. water) blocks.
+Pixels drawn blend into existing geometry.*/
+void MapRenderer_RenderTranslucent(Real64 deltaTime);
+
+static void MapRenderer_CheckWeather(Real64 deltaTime);
+
+static void MapRenderer_RenderNormalBatch(Int32 batch);
+
+static void MapRenderer_RenderTranslucentBatch(Int32 batch);
+#endif
--- src/Client/TerrainAtlas1D.h
+++ src/Client/TerrainAtlas1D.h
@@ -4,13 +4,16 @@
 #include "TextureRec.h"
 #include "TerrainAtlas2D.h"
 /* Represents 2D terrain.png converted into an array of 1D textures.
-   Copyright 2014-2017 ClassicalSharp | Licensed under BSD-3
+Copyright 2014-2017 ClassicalSharp | Licensed under BSD-3
 */
+
+/* The theoretical largest number of 1D atlases that a 2D atlas can be broken down into. */
+#define Atlas1D_MaxAtlasesCount (Atlas2D_ElementsPerRow * Atlas2D_RowsCount)
 
 /* The number of elements each 1D atlas contains. */
 Int32 Atlas1D_ElementsPerAtlas;
 
-/* The number of elements in each adjusted 1D atlas. 
+/* The number of elements in each adjusted 1D atlas.
 (in the case only 1 1D atlas used, and is resized vertically to be a power of two) */
 Int32 Atlas1D_ElementsPerBitmap;
 
@@ -18,17 +21,17 @@ Int32 Atlas1D_ElementsPerBitmap;
 Real32 Atlas1D_InvElementSize;
 
 /* Native texture ID for each 1D atlas. */
-Int32 Atlas1D_TexIds[Atlas2D_ElementsPerRow * Atlas2D_RowsCount];
+Int32 Atlas1D_TexIds[Atlas1D_MaxAtlasesCount];
 Int32 Atlas1D_TexIdsCount;
 
 /* Retrieves the 1D texture rectangle and 1D atlas index of the given texture. */
-TextureRec Atlas1D_TexRec(TextureLoc texLoc, Int32 uCount, Int32* index);
+TextureRec Atlas1D_TexRec(Int32 texId, Int32 uCount, Int32* index);
 
-/* Returns the index of the 1D atlas within the array of 1D atlases that contains the given texture location.*/
-#define Atlas1D_Index(texLoc) (texLoc) / Atlas1D_ElementsPerAtlas
+/* Returns the index of the 1D atlas within the array of 1D atlases that contains the given texture id.*/
+#define Atlas1D_Index(texId) (texId) / Atlas1D_ElementsPerAtlas
 
-/* Returns the index of the given texture location within a 1D atlas. */
-#define Atlas1D_RowId(texLoc) (texLoc) % Atlas1D_ElementsPerAtlas
+/* Returns the index of the given texture id within a 1D atlas. */
+#define Atlas1D_RowId(texId) (texId) % Atlas1D_ElementsPerAtlas
 
 /* Updates variables and native textures for the 1D atlas array. */
 void Atlas1D_UpdateState();
