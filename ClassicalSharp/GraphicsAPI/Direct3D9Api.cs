b7929e541d2a0412efa20f989f4e44efab26a3c6
UnknownShadow200 <unknownshadow200@gmail.com>
2017-06-05 15:32:48 +1000

    fix last commit

--- ClassicalSharp/GraphicsAPI/Direct3D9Api.cs
+++ ClassicalSharp/GraphicsAPI/Direct3D9Api.cs
@@ -376,7 +376,7 @@ namespace ClassicalSharp.GraphicsAPI {
 			}
 
 			public void MultiplyTop(ref Matrix4 matrix) {
-				Matrix4.Mult(ref matrix, ref stack[stackIndex], out stack[stackIndex]); // top = matrix * top
+				Matrix4.Mult(out stack[stackIndex], ref matrix, ref stack[stackIndex]); // top = matrix * top
 				device.SetTransform(matrixType, ref stack[stackIndex]);
 			}
 
