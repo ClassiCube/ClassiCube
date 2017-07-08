027834501829a2ae1409eb3862290f31a9cfa387
UnknownShadow200 <unknownshadow200@gmail.com>
2017-07-09 00:13:52 +1000

    Move MessageBox.Show call out of Program.CheckFilesExist, partially fixes being unable to run launcher when system.windows.forms dll is missing

--- Launcher2/Program.cs
+++ Launcher2/Program.cs
@@ -27,17 +27,17 @@ namespace Launcher {
 		
 		static bool CheckFilesExist() {
 			string path = Path.Combine(AppDirectory, "ClassicalSharp.exe");
-			if (!File.Exists(path)) {
-				MessageBox.Show("ClassicalSharp.exe needs to be in the same folder as the launcher.", "Missing file");
-				return false;
-			}
-			
+			if (!File.Exists(path)) { MessageMissing("ClassicalSharp.exe"); return false; }
+
 			path = Path.Combine(AppDirectory, "OpenTK.dll");
-			if (!File.Exists(path)) {
-				MessageBox.Show("OpenTK.dll needs to be in the same folder as the launcher.", "Missing file");
-				return false;
-			}
+			if (!File.Exists(path)) { MessageMissing("OpenTK.dll"); return false; }
+			
 			return true;		
+		}
+		
+		// put in separate function, because we don't want to load winforms assembly if possible
+		static void MessageMissing(string file) {
+			MessageBox.Show(file + " needs to be in the same folder as the launcher.", "Missing file");
 		}
 		
 		static void RunLauncher() {
