bee175eb7cfd6dd87e941a81350a6ae76f9595d7
UnknownShadow200 <unknownshadow200@gmail.com>
2017-10-13 12:10:03 +1100

    Eliminate generics usage in Particle classes

--- ClassicalSharp/Particles/Particle.cs
+++ ClassicalSharp/Particles/Particle.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright 2014-2017 ClassicalSharp | Licensed under BSD-3
 using System;
+using ClassicalSharp.Entities;
 using ClassicalSharp.GraphicsAPI;
 using ClassicalSharp.Map;
 using OpenTK;
@@ -12,11 +13,11 @@ using BlockID = System.Byte;
 
 namespace ClassicalSharp.Particles {
 
-	public abstract class Particle {
-		
+	public abstract class Particle {		
 		public Vector3 Velocity;
 		public float Lifetime;
 		protected Vector3 lastPos, nextPos;
+		protected bool hitTerrain = false, throughLiquids = true;
 		
 		// http://www.opengl-tutorial.org/intermediate-tutorials/billboards-particles/billboards/
 		public static void DoRender(Game g, ref Vector2 size, ref Vector3 pos, ref TextureRec rec,
@@ -41,13 +42,91 @@ namespace ClassicalSharp.Particles {
 			              v.V = rec.V2; vertices[index++] = v;
 		}
 		
-		public virtual bool Tick(Game game, double delta) {
+		public void ResetState(Vector3 pos, Vector3 velocity, double lifetime) {
+			lastPos = nextPos = pos;
+			Velocity = velocity;
+			Lifetime = (float)lifetime;
+			hitTerrain = false;
+		}
+
+		protected bool Tick(Game game, float gravity, double delta) {
+			hitTerrain = false;
+			lastPos = nextPos;
+			BlockID curBlock = GetBlock(game, (int)nextPos.X, (int)nextPos.Y, (int)nextPos.Z);
+			float minY = Utils.Floor(nextPos.Y) + BlockInfo.MinBB[curBlock].Y;
+			float maxY = Utils.Floor(nextPos.Y) + BlockInfo.MaxBB[curBlock].Y;			
+			if (!CanPassThrough(game, curBlock) && nextPos.Y >= minY && nextPos.Y < maxY && CollideHor(game, curBlock))
+				return true;
+			
+			Velocity.Y -= gravity * (float)delta;
+			int startY = (int)Math.Floor(nextPos.Y);
+			nextPos += Velocity * (float)delta * 3;
+			int endY = (int)Math.Floor(nextPos.Y);
+			
+			if (Velocity.Y > 0) {
+				// don't test block we are already in
+				for (int y = startY + 1; y <= endY && TestY(game, y, false); y++);
+			} else {
+				for (int y = startY; y >= endY && TestY(game, y, true); y--);
+			}
+			
 			Lifetime -= (float)delta;
 			return Lifetime < 0;
+		}	
+		
+		bool TestY(Game game, int y, bool topFace) {
+			if (y < 0) {
+				nextPos.Y = lastPos.Y = 0 + Entity.Adjustment;
+				Velocity = Vector3.Zero;
+				hitTerrain = true;
+				return false;
+			}
+			
+			BlockID block = GetBlock(game, (int)nextPos.X, y, (int)nextPos.Z);
+			if (CanPassThrough(game, block)) return true;
+			Vector3 minBB = BlockInfo.MinBB[block];
+			Vector3 maxBB = BlockInfo.MaxBB[block];
+			float collideY = y + (topFace ? maxBB.Y : minBB.Y);
+			bool collideVer = topFace ? (nextPos.Y < collideY) : (nextPos.Y > collideY);
+			
+			if (collideVer && CollideHor(game, block)) {
+				float adjust = topFace ? Entity.Adjustment : -Entity.Adjustment;
+				nextPos.Y = lastPos.Y = collideY + adjust;
+				Velocity = Vector3.Zero;
+				hitTerrain = true;
+				return false;
+			}
+			return true;
+		}
+		
+		bool CanPassThrough(Game game, BlockID block) {
+			byte draw = BlockInfo.Draw[block];
+			return draw == DrawType.Gas || draw == DrawType.Sprite
+				|| (throughLiquids && BlockInfo.IsLiquid(block));
+		}
+		
+		bool CollideHor(Game game, BlockID block) {
+			Vector3 min = BlockInfo.MinBB[block] + FloorHor(nextPos);
+			Vector3 max = BlockInfo.MaxBB[block] + FloorHor(nextPos);
+			return nextPos.X >= min.X && nextPos.Z >= min.Z &&
+				nextPos.X < max.X && nextPos.Z < max.Z;
+		}
+		
+		BlockID GetBlock(Game game, int x, int y, int z) {
+			if (game.World.IsValidPos(x, y, z))
+				return game.World.GetBlock(x, y, z);
+			
+			if (y >= game.World.Env.EdgeHeight) return Block.Air;
+			if (y >= game.World.Env.SidesHeight) return game.World.Env.EdgeBlock;
+			return game.World.Env.SidesBlock;
+		}
+		
+		static Vector3 FloorHor(Vector3 v) {
+			return new Vector3(Utils.Floor(v.X), 0, Utils.Floor(v.Z));
 		}
 	}
 	
-	public sealed class RainParticle : CollidableParticle {
+	public sealed class RainParticle : Particle {
 		
 		static Vector2 bigSize = new Vector2(1/16f, 1/16f);
 		static Vector2 smallSize = new Vector2(0.75f/16f, 0.75f/16f);
@@ -58,7 +137,7 @@ namespace ClassicalSharp.Particles {
 		
 		public bool Big, Tiny;
 		
-		public override bool Tick(Game game, double delta) {
+		public bool Tick(Game game, double delta) {
 			bool dies = Tick(game, 3.5f, delta);
 			return hitTerrain ? true : dies;
 		}
@@ -73,14 +152,14 @@ namespace ClassicalSharp.Particles {
 		}
 	}
 	
-	public sealed class TerrainParticle : CollidableParticle {
+	public sealed class TerrainParticle : Particle {
 		
 		static Vector2 terrainSize = new Vector2(1/8f, 1/8f);
 		internal TextureRec rec;
 		internal byte texLoc;
 		internal BlockID block;
 		
-		public override bool Tick(Game game, double delta) {
+		public bool Tick(Game game, double delta) {
 			return Tick(game, 5.4f, delta);
 		}
 		
--- ClassicalSharp/Particles/ParticleManager.cs
+++ ClassicalSharp/Particles/ParticleManager.cs
@@ -2,10 +2,17 @@
 using System;
 using ClassicalSharp.Events;
 using ClassicalSharp.GraphicsAPI;
+using OpenTK;
+
+#if USE16_BIT
+using BlockID = System.UInt16;
+#else
+using BlockID = System.Byte;
+#endif
 
 namespace ClassicalSharp.Particles {
 	
-	public partial class ParticleManager : IGameComponent {
+	public sealed class ParticleManager : IGameComponent {
 		
 		public int ParticlesTexId;
 		TerrainParticle[] terrainParticles = new TerrainParticle[maxParticles];
@@ -43,6 +50,7 @@ namespace ClassicalSharp.Particles {
 			if (e.Name == "particles.png")
 				game.UpdateTexture(ref ParticlesTexId, e.Name, e.Data, false);
 		}
+		
 		
 		VertexP3fT2fC4b[] vertices = new VertexP3fT2fC4b[0];
 		public void Render(double delta, float t) {
@@ -117,20 +125,146 @@ namespace ClassicalSharp.Particles {
 			gfx.UpdateDynamicVb_IndexedTris(vb, vertices, drawCount);
 		}
 		
+		
 		public void Tick(ScheduledTask task) {
-			TickParticles(terrainParticles, ref terrainCount, task.Interval);
-			TickParticles(rainParticles, ref rainCount, task.Interval);
+			TickTerrainParticles(task.Interval);
+			TickRainParticles(task.Interval);
 		}
 		
-		void TickParticles(Particle[] particles, ref int count, double delta) {
-			for (int i = 0; i < count; i++) {
-				Particle particle = particles[i];
-				if (particle.Tick(game, delta)) {
-					RemoveAt(i, particles, ref count);
-					i--;
+		void TickTerrainParticles(double delta) {
+			for (int i = 0; i < terrainCount; i++) {
+				if (terrainParticles[i].Tick(game, delta)) {
+					RemoveTerrainAt(i); i--;
 				}
 			}
 		}
+		
+		void RemoveTerrainAt(int index) {
+			TerrainParticle removed = terrainParticles[index];
+			for (int i = index; i < terrainCount - 1; i++) {
+				terrainParticles[i] = terrainParticles[i + 1];
+			}
+			terrainParticles[terrainCount - 1] = removed;
+			terrainCount--;
+		}
+		
+		void TickRainParticles(double delta) {
+			for (int i = 0; i < rainCount; i++) {
+				if (rainParticles[i].Tick(game, delta)) {
+					RemoveRainAt(i); i--;
+				}
+			}
+		}
+		
+		void RemoveRainAt(int index) {
+			RainParticle removed = rainParticles[index];
+			for (int i = index; i < rainCount - 1; i++) {
+				rainParticles[i] = rainParticles[i + 1];
+			}
+			rainParticles[rainCount - 1] = removed;
+			rainCount--;
+		}		
+		
+		
+		void BreakBlockEffect(object sender, BlockChangedEventArgs e) {
+			if (e.Block != Block.Air) return;
+			Vector3I position = e.Coords;
+			BlockID block = e.OldBlock;
+			
+			Vector3 worldPos = new Vector3(position.X, position.Y, position.Z);
+			int texLoc = BlockInfo.GetTextureLoc(block, Side.Left), texIndex = 0;
+			TextureRec baseRec = game.TerrainAtlas1D.GetTexRec(texLoc, 1, out texIndex);
+			float uScale = (1/16f), vScale = (1/16f) * game.TerrainAtlas1D.invElementSize;
+			
+			Vector3 minBB = BlockInfo.MinBB[block];
+			Vector3 maxBB = BlockInfo.MaxBB[block];
+			int minU = Math.Min((int)(minBB.X * 16), (int)(minBB.Z * 16));
+			int maxU = Math.Min((int)(maxBB.X * 16), (int)(maxBB.Z * 16));
+			int minV = (int)(16 - maxBB.Y * 16), maxV = (int)(16 - minBB.Y * 16);
+			int maxUsedU = maxU, maxUsedV = maxV;
+			// This way we can avoid creating particles which outside the bounds and need to be clamped
+			if (minU < 12 && maxU > 12) maxUsedU = 12;
+			if (minV < 12 && maxV > 12) maxUsedV = 12;
+			
+			const int gridSize = 4;
+			// gridOffset gives the centre of the cell on a grid
+			const float cellCentre = (1f / gridSize) * 0.5f;
+			
+			for (int x = 0; x < gridSize; x++)
+				for (int y = 0; y < gridSize; y++)
+					for (int z = 0; z < gridSize; z++)
+			{
+				float cellX = (float)x / gridSize, cellY = (float)y / gridSize, cellZ = (float)z / gridSize;
+				Vector3 cell = new Vector3(cellCentre + cellX, cellCentre / 2 + cellY, cellCentre + cellZ);
+				if (cell.X < minBB.X || cell.X > maxBB.X || cell.Y < minBB.Y
+				    || cell.Y > maxBB.Y || cell.Z < minBB.Z || cell.Z > maxBB.Z) continue;
+				
+				double velX = cellCentre + (cellX - 0.5f) + (rnd.NextDouble() * 0.4 - 0.2); // centre random offset around [-0.2, 0.2]
+				double velY = cellCentre + (cellY - 0.0f) + (rnd.NextDouble() * 0.4 - 0.2);
+				double velZ = cellCentre + (cellZ - 0.5f) + (rnd.NextDouble() * 0.4 - 0.2);
+				Vector3 velocity = new Vector3((float)velX, (float)velY, (float)velZ);
+				
+				TextureRec rec = baseRec;
+				rec.U1 = baseRec.U1 + rnd.Next(minU, maxUsedU) * uScale;
+				rec.V1 = baseRec.V1 + rnd.Next(minV, maxUsedV) * vScale;
+				rec.U2 = Math.Min(baseRec.U1 + maxU * uScale, rec.U1 + 4 * uScale) - 0.01f * uScale;
+				rec.V2 = Math.Min(baseRec.V1 + maxV * vScale, rec.V1 + 4 * vScale) - 0.01f * vScale;
+				double life = 0.3 + rnd.NextDouble() * 1.2;
+				
+				TerrainParticle p = GetTerrainParticle();
+				p.ResetState(worldPos + cell, velocity, life);
+				p.rec = rec;
+				
+				p.texLoc = (byte)texLoc;
+				p.block = block;
+			}
+		}
+		
+		TerrainParticle GetTerrainParticle() {
+			if (terrainCount == maxParticles) RemoveTerrainAt(0);
+			terrainCount++;
+			
+			TerrainParticle particle = terrainParticles[terrainCount - 1];
+			if (particle != null) return particle;
+			
+			particle = new TerrainParticle();
+			terrainParticles[terrainCount - 1] = particle;
+			return particle;
+		}
+		
+		public void AddRainParticle(Vector3 pos) {
+			Vector3 startPos = pos;
+			for (int i = 0; i < 2; i++) {
+				double velX = rnd.NextDouble() * 0.8 - 0.4; // [-0.4, 0.4]
+				double velZ = rnd.NextDouble() * 0.8 - 0.4;
+				double velY = rnd.NextDouble() + 0.4;
+				Vector3 velocity = new Vector3((float)velX, (float)velY, (float)velZ);
+				
+				double xOffset = rnd.NextDouble() - 0.5; // [-0.5, 0.5]
+				double yOffset = rnd.NextDouble() * 0.1 + 0.01;
+				double zOffset = rnd.NextDouble() - 0.5;
+				pos = startPos + new Vector3(0.5f + (float)xOffset,
+				                             (float)yOffset, 0.5f + (float)zOffset);
+				double life = 40;
+				RainParticle p = GetRainParticle();
+				p.ResetState(pos, velocity, life);
+				p.Big = rnd.Next(0, 20) >= 18;
+				p.Tiny = rnd.Next(0, 30) >= 28;
+			}
+		}
+		
+		RainParticle GetRainParticle() {
+			if (rainCount == maxParticles) RemoveRainAt(0);
+			rainCount++;
+			
+			RainParticle particle = rainParticles[rainCount - 1];
+			if (particle != null) return particle;
+			
+			particle = new RainParticle();
+			rainParticles[rainCount - 1] = particle;
+			return particle;
+		}
+		
 		
 		public void Dispose() {
 			game.Graphics.DeleteTexture(ref ParticlesTexId);
@@ -141,16 +275,6 @@ namespace ClassicalSharp.Particles {
 			ContextLost();
 			game.Graphics.ContextLost -= ContextLost;
 			game.Graphics.ContextRecreated -= ContextRecreated;
-		}
-		
-		void RemoveAt<T>(int index, T[] particles, ref int count) where T : Particle {
-			T removed = particles[index];
-			for (int i = index; i < count - 1; i++) {
-				particles[i] = particles[i + 1];
-			}
-			
-			particles[count - 1] = removed;
-			count--;
 		}
 		
 		void ContextLost() { game.Graphics.DeleteVb(ref vb); }
