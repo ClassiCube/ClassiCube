064c7a2514f7ccd775a1b44f90b7e2c1e512d766
UnknownShadow200 <unknownshadow200@gmail.com>
2017-11-13 16:36:45 +1100

    Release 0.99.9.7

--- Launcher2/Program.cs
+++ Launcher2/Program.cs
@@ -8,7 +8,7 @@ namespace Launcher {
 
 	internal static class Program {
 		
-		public const string AppName = "ClassicalSharp Launcher 0.99.9.6";
+		public const string AppName = "ClassicalSharp Launcher 0.99.9.7";
 		
 		public static string AppDirectory;
 		
