29bca54ef2a564ac1ab4e2bedee91e069bb563eb
UnknownShadow200 <unknownshadow200@gmail.com>
2018-06-03 22:47:54 +1000

    Release 0.99.9.95

--- ClassicalSharp/Program.cs
+++ ClassicalSharp/Program.cs
@@ -9,7 +9,7 @@ namespace ClassicalSharp {
 	
 	internal static class Program {
 		
-		public const string AppName = "ClassicalSharp 0.99.9.94";
+		public const string AppName = "ClassicalSharp 0.99.9.95";
 		
 		#if !LAUNCHER
 		[STAThread]
--- Launcher2/Program.cs
+++ Launcher2/Program.cs
@@ -8,7 +8,7 @@ namespace Launcher {
 
 	internal static class Program {
 		
-		public const string AppName = "ClassicalSharp Launcher 0.99.9.94";
+		public const string AppName = "ClassicalSharp Launcher 0.99.9.95";
 		
 		public static bool ShowingErrorDialog = false;
 		
