88652639107e510e8fa1769085db359f7564eeed
UnknownShadow200 <unknownshadow200@gmail.com>
2018-11-25 07:05:00 +1100

    Release 0.99.9.98 to fix classic+hax mode

--- Launcher2/Program.cs
+++ Launcher2/Program.cs
@@ -6,7 +6,7 @@ namespace Launcher {
 
 	internal static class Program {
 		
-		public const string AppName = "ClassicalSharp Launcher 0.99.9.97";
+		public const string AppName = "ClassicalSharp Launcher 0.99.9.98";
 		public static bool ShowingErrorDialog = false;
 		
 		[STAThread]
