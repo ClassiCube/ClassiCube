e2035f3ae55826f59c5cb09356ab27f3ea0a09b5
UnknownShadow200 <unknownshadow200@gmail.com>
2018-12-03 19:36:50 +1100

    Release 0.99.9.99

--- ClassicalSharp/Program.cs
+++ ClassicalSharp/Program.cs
@@ -8,7 +8,7 @@ namespace ClassicalSharp {
 	
 	internal static class Program {
 		
-		public const string AppName = "ClassicalSharp 0.99.9.98";
+		public const string AppName = "ClassicalSharp 0.99.9.99";
 		
 		public const int APIVersion = 2;
 		
