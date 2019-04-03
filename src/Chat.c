19520c205d026925851dc1cb9260680af3b4cbdd
UnknownShadow200 <unknownshadow200@gmail.com>
2019-04-03 17:26:17 +1100

    Just doing /client resolution should tell you current resolution (thanks masoncb)

--- src/Chat.c
+++ src/Chat.c
@@ -371,7 +371,7 @@ static struct ChatCommand RenderTypeCommand = {
 static void ResolutionCommand_Execute(const String* args, int argsCount) {
 	int width, height;
 	if (argsCount < 2) {
-		Chat_AddRaw("&e/client: &cYou didn't specify width and height");
+		Chat_Add2("&e/client: &fCurrent resolution is %i x %i", &Window_ClientBounds.Width, &Window_ClientBounds.Height);
 	} else if (!Convert_ParseInt(&args[0], &width) || !Convert_ParseInt(&args[1], &height)) {
 		Chat_AddRaw("&e/client: &cWidth and height must be integers.");
 	} else if (width <= 0 || height <= 0) {
