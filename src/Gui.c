7f0d895a913959c081fb0c89e667f5cbe435d22a
UnknownShadow200 <unknownshadow200@gmail.com>
2019-05-06 08:01:50 +1000

    Clickable chat and tab complete on by default in enhanced mode

--- src/Gui.c
+++ src/Gui.c
@@ -97,8 +97,8 @@ CC_NOINLINE static void Gui_RecreateScreen(struct Screen* screen) {
 
 static void Gui_LoadOptions(void) {
 	Gui_Chatlines       = Options_GetInt(OPT_CHATLINES, 0, 30, 12);
-	Gui_ClickableChat   = Options_GetBool(OPT_CLICKABLE_CHAT, false);
-	Gui_TabAutocomplete = Options_GetBool(OPT_TAB_AUTOCOMPLETE, false);
+	Gui_ClickableChat   = Options_GetBool(OPT_CLICKABLE_CHAT,   !Game_ClassicMode);
+	Gui_TabAutocomplete = Options_GetBool(OPT_TAB_AUTOCOMPLETE, !Game_ClassicMode);
 
 	Gui_ClassicTexture = Options_GetBool(OPT_CLASSIC_GUI, true)      || Game_ClassicMode;
 	Gui_ClassicTabList = Options_GetBool(OPT_CLASSIC_TABLIST, false) || Game_ClassicMode;
