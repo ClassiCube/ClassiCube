49152ee5d99bb364491aff5436f203c97080e1a6
UnknownShadow200 <unknownshadow200@gmail.com>
2019-08-23 20:38:19 +1000

    Remove some unused freetype code

--- src/freetype/ftobjs.c
+++ src/freetype/ftobjs.c
@@ -3940,37 +3940,6 @@
   }
 
 
-  /* documentation is in freetype.h */
-
-  FT_EXPORT_DEF( void )
-  FT_Library_Version( FT_Library   library,
-                      FT_Int      *amajor,
-                      FT_Int      *aminor,
-                      FT_Int      *apatch )
-  {
-    FT_Int  major = 0;
-    FT_Int  minor = 0;
-    FT_Int  patch = 0;
-
-
-    if ( library )
-    {
-      major = library->version_major;
-      minor = library->version_minor;
-      patch = library->version_patch;
-    }
-
-    if ( amajor )
-      *amajor = major;
-
-    if ( aminor )
-      *aminor = minor;
-
-    if ( apatch )
-      *apatch = patch;
-  }
-
-
   /* documentation is in ftmodapi.h */
 
   FT_EXPORT_DEF( FT_Error )
