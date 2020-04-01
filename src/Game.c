21e6c8431da193d1a883e4a4981725fcf459f717
UnknownShadow200 <unknownshadow200@gmail.com>
2020-04-01 20:21:38 +1100

    Fix initial map generation of singleplayer, move EnvRenderer_UpdateFog to top of Render3D

--- src/Game.c
+++ src/Game.c
@@ -508,15 +508,15 @@ static void UpdateViewMatrix(void) {
 static void Game_Render3D(double delta, float t) {
 	Vec3 pos;
 
+	EnvRenderer_UpdateFog();
 	if (EnvRenderer_ShouldRenderSkybox()) EnvRenderer_RenderSkybox();
+
 	AxisLinesRenderer_Render();
 	Entities_RenderModels(delta, t);
 	Entities_RenderNames();
 
 	Particles_Render(t);
 	Camera.Active->GetPickedBlock(&Game_SelectedPos); /* TODO: only pick when necessary */
-
-	EnvRenderer_UpdateFog();
 	EnvRenderer_RenderSky();
 	EnvRenderer_RenderClouds();
 
