f6b6c5d2f22b24ff85a7e444aa411adf6f2611b5
UnknownShadow200 <unknownshadow200@gmail.com>
2020-05-03 23:16:32 +1000

    Get rid of some of commented out OLD_ENGINE code

--- src/freetype/ftdriver.h
+++ src/freetype/ftdriver.h
@@ -143,29 +143,6 @@ FT_BEGIN_HEADER
   /**************************************************************************
    *
    * @section:
-   *   pcf_driver
-   *
-   * @title:
-   *   The PCF driver
-   *
-   * @abstract:
-   *   Controlling the PCF driver module.
-   *
-   * @description:
-   *   While FreeType's PCF driver doesn't expose API functions by itself,
-   *   it is possible to control its behaviour with @FT_Property_Set and
-   *   @FT_Property_Get.  Right now, there is a single property
-   *   @no-long-family-names available if FreeType is compiled with
-   *   PCF_CONFIG_OPTION_LONG_FAMILY_NAMES.
-   *
-   *   The PCF driver's module name is `pcf'.
-   *
-   */
-
-
-  /**************************************************************************
-   *
-   * @section:
    *   t1_cid_driver
    *
    * @title:
@@ -565,49 +542,6 @@ FT_BEGIN_HEADER
    *
    *   2.9 (for `type1' and `t1cid' modules)
    *
-   */
-
-
-  /**************************************************************************
-   *
-   * @property:
-   *   no-long-family-names
-   *
-   * @description:
-   *   If PCF_CONFIG_OPTION_LONG_FAMILY_NAMES is active while compiling
-   *   FreeType, the PCF driver constructs long family names.
-   *
-   *   There are many PCF fonts just called `Fixed' which look completely
-   *   different, and which have nothing to do with each other.  When
-   *   selecting `Fixed' in KDE or Gnome one gets results that appear rather
-   *   random, the style changes often if one changes the size and one
-   *   cannot select some fonts at all.  The improve this situation, the PCF
-   *   module prepends the foundry name (plus a space) to the family name.
-   *   It also checks whether there are `wide' characters; all put together,
-   *   family names like `Sony Fixed' or `Misc Fixed Wide' are constructed.
-   *
-   *   If `no-long-family-names' is set, this feature gets switched off.
-   *
-   *   {
-   *     FT_Library  library;
-   *     FT_Bool     no_long_family_names = TRUE;
-   *
-   *
-   *     FT_Init_FreeType( &library );
-   *
-   *     FT_Property_Set( library, "pcf",
-   *                               "no-long-family-names",
-   *                               &no_long_family_names );
-   *   }
-   *
-   * @note:
-   *   This property can be used with @FT_Property_Get also.
-   *
-   *   This property can be set via the `FREETYPE_PROPERTIES' environment
-   *   variable (using values 1 and 0 for `on' and `off', respectively).
-   *
-   * @since:
-   *   2.8
    */
 
 
