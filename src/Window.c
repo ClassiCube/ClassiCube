7694980c0f93d1538c564bddf03f811ea870ec36
UnknownShadow200 <unknownshadow200@gmail.com>
2020-08-05 19:58:39 +1000

    Readd the call to XSetWMNormalHints I removed to fix appearing at wrong position with some window managers

--- src/Window.c
+++ src/Window.c
@@ -1158,6 +1158,7 @@ static void ApplyIcon(void) { }
 
 void Window_Create(int width, int height) {
 	XSetWindowAttributes attributes = { 0 };
+	XSizeHints hints = { 0 };
 	Atom protocols[2];
 	struct GraphicsMode mode;
 	cc_uintptr addr;
@@ -1186,6 +1187,11 @@ void Window_Create(int width, int height) {
 		0, win_visual.depth /* CopyFromParent*/, InputOutput, win_visual.visual, 
 		CWColormap | CWEventMask | CWBackPixel | CWBorderPixel, &attributes);
 	if (!win_handle) Logger_Abort("XCreateWindow failed");
+
+	hints.base_width  = width;
+	hints.base_height = height;
+	hints.flags = PSize | PPosition;
+	XSetWMNormalHints(win_display, win_handle, &hints);
 
 	/* Register for window destroy notification */
 	protocols[0] = wm_destroy;
