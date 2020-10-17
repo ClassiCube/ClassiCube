851a58d743b48dabd0813e5834c29da0d4d64037
UnknownShadow200 <unknownshadow200@gmail.com>
2020-10-17 19:32:42 +1100

    Add support for reducing a few emoji into code page 437 characters, mostly just variants of the happy one
    This has only been tested with mobile web client and probably doesn't work anywhere else

--- src/String.c
+++ src/String.c
@@ -507,9 +507,20 @@ char Convert_CodepointToCP437(cc_codepoint cp) {
 	char c; Convert_TryCodepointToCP437(cp, &c); return c;
 }
 
+static cc_codepoint ReduceEmoji(cc_codepoint cp) {
+	if (cp == 0x1F31E) return 0x263C;
+	if (cp == 0x1F3B5) return 0x266B;
+	if (cp == 0x1F642) return 0x263A;
+
+	if (cp == 0x1F600 || cp == 0x1F601 || cp == 0x1F603) return 0x263A;
+	if (cp == 0x1F604 || cp == 0x1F606 || cp == 0x1F60A) return 0x263A;
+	return cp;
+}
+
 cc_bool Convert_TryCodepointToCP437(cc_codepoint cp, char* c) {
 	int i;
 	if (cp >= 0x20 && cp < 0x7F) { *c = (char)cp; return true; }
+	if (cp >= 0x1F000) cp = ReduceEmoji(cp);
 
 	for (i = 0; i < Array_Elems(controlChars); i++) {
 		if (controlChars[i] == cp) { *c = i; return true; }
