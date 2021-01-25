12e675f7548bb883aedf2d00f22ed7ea55a2396d
UnknownShadow200 <unknownshadow200@gmail.com>
2021-01-25 23:42:31 +1100

    Add buildbot script for android (still got a few things to fix though)

---
+++ misc/builddroid.sh
@@ -0,0 +1,44 @@
+FLAGS="-fPIC -shared -s -O1"
+LIBS="-lGLESv2 -lEGL -lOpenSLES -lm -landroid -llog"
+NDK_ROOT="/home/buildbot/android/android-ndk-r22/toolchains/llvm/prebuilt/linux-x86_64/bin"
+TOOLS_ROOT="/home/buildbot/android/sdk/build-tools/26.0.0"
+SDK_ROOT="/home/buildbot/android/sdk/platforms/android-26"
+
+cd /home/buildbot/client/src
+$NDK_ROOT/i686-linux-android26-clang *.c $FLAGS $LIBS -o cc-droid-x86_32
+$NDK_ROOT/aarch64-linux-android26-clang *.c $FLAGS $LIBS -o cc-droid-arm_64
+$NDK_ROOT/x86_64-linux-android26-clang *.c $FLAGS $LIBS -o cc-droid-x86_64
+$NDK_ROOT/armv7a-linux-androideabi26-clang *.c $FLAGS $LIBS -o cc-droid-arm_32
+
+cd ../android/app/src/main
+# remove old java temp files
+rm -rf obj
+mkdir obj
+rm classes.dex
+
+# copy required native libraries
+rm -rf lib
+mkdir lib
+mkdir lib/armeabi-v7a
+mkdir lib/arm64-v8a
+mkdir lib/x86
+mkdir lib/x86_64
+cp ~/client/src/cc-droid-arm_32 lib/armeabi-v7a/libclassicube.so
+cp ~/client/src/cc-droid-arm_64 lib/arm64-v8a/libclassicube.so
+cp ~/client/src/cc-droid-x86_32 lib/x86/libclassicube.so
+cp ~/client/src/cc-droid-x86_64 lib/x86_64/libclassicube.so
+
+# compile interop java file into its multiple .class files
+javac java/com/classicube/MainActivity.java -d ./obj -classpath $SDK_ROOT/android.jar
+# compile the multiple .class files into one .dex file
+$TOOLS_ROOT/dx --dex --output=obj/classes.dex ./obj
+# create initial .apk with packaged version of resources
+$TOOLS_ROOT/aapt package -f -M AndroidManifest.xml -S res -F obj/cc-unsigned.apk -I $SDK_ROOT/android.jar
+# and add all the required files
+cp obj/classes.dex classes.dex
+$TOOLS_ROOT/aapt add -f obj/cc-unsigned.apk classes.dex lib/armeabi-v7a/libclassicube.so lib/arm64-v8a/libclassicube.so lib/x86/libclassicube.so lib/x86_64/libclassicube.so
+# sign the apk with debug key (https://stackoverflow.com/questions/16711233/)
+cp obj/cc-unsigned.apk obj/cc-signed.apk
+jarsigner -verbose -keystore debug.keystore -storepass android -keypass android obj/cc-signed.apk androiddebugkey
+# create aligned .apk file
+$TOOLS_ROOT/zipalign -f -v 4 obj/cc-signed.apk obj/cc-final.apk
