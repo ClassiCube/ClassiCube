f81f96d6f42da2adbbd92b614f939b1b8bb5f53c
UnknownShadow200 <unknownshadow200@gmail.com>
2022-10-08 13:32:26 +1100

    un-un-un-un-un-un-re-enable ModernGL build

--- src/Platform_Posix.c
+++ src/Platform_Posix.c
@@ -789,27 +789,19 @@ cc_bool Updater_Clean(void) { return true; }
 #elif defined CC_BUILD_LINUX
 	#if __x86_64__
 	const struct UpdaterInfo Updater_Info = {
-		#ifndef CC_BUILD_GLMODERN
-		"", 1, { { "OpenGL", "ClassiCube" } }
-		#else		
 		"&eModernGL is recommended for newer machines (2010 or later)", 2,
 		{
 			{ "ModernGL", "cc-nix64-gl2" },
 			{ "OpenGL",   "ClassiCube" }
 		}
-		#endif
 	};
 	#elif __i386__
 	const struct UpdaterInfo Updater_Info = {
-		#ifndef CC_BUILD_GLMODERN
-		"", 1, { { "OpenGL", "ClassiCube.32" } }
-		#else
 		"&eModernGL is recommended for newer machines (2010 or later)", 2,
 		{
 			{ "ModernGL", "cc-nix32-gl2" },
 			{ "OpenGL",   "ClassiCube.32" }
 		}
-		#endif
 	};
 	#else
 	const struct UpdaterInfo Updater_Info = { "&eCompile latest source code to update", 0 };
@@ -817,27 +809,19 @@ cc_bool Updater_Clean(void) { return true; }
 #elif defined CC_BUILD_MACOS
 	#if __x86_64__
 	const struct UpdaterInfo Updater_Info = {
-		#ifndef CC_BUILD_GLMODERN
-		"", 1, { { "OpenGL", "ClassiCube.64.osx" } }
-		#else
 		"&eModernGL is recommended for newer machines (2010 or later)", 2,
 		{
 			{ "ModernGL", "cc-osx64-gl2" },
 			{ "OpenGL",   "ClassiCube.64.osx" }
 		}
-		#endif
 	};
 	#elif __i386__
 	const struct UpdaterInfo Updater_Info = {
-		#ifndef CC_BUILD_GLMODERN
-		"", 1, { { "OpenGL", "ClassiCube.osx" } }
-		#else
 		"&eModernGL is recommended for newer machines (2010 or later)", 2,
 		{
 			{ "ModernGL", "cc-osx32-gl2" },
 			{ "OpenGL",   "ClassiCube.osx" }
 		}
-		#endif
 	};
 	#else
 	const struct UpdaterInfo Updater_Info = { "&eCompile latest source code to update", 0 };
