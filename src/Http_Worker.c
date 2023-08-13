ClassiCube/ClassiCube

commit b5acd75c3e1dc8cbf9f9dc24388b25e3cff737c1

Move app name platform suffixes out of Constants.h and into the respective platform backends instead

src/Protocol.c
@@ -95,7 +95,7 @@ static struct CpeExt* cpe_clientExtensions[] = {
 	&envColors_Ext, &selectionCuboid_Ext, &blockPerms_Ext, &changeModel_Ext, &mapAppearance_Ext, &weatherType_Ext,
 	&messageTypes_Ext, &hackControl_Ext, &playerClick_Ext, &fullCP437_Ext, &longerMessages_Ext, &blockDefs_Ext,
 	&blockDefsExt_Ext, &bulkBlockUpdate_Ext, &textColors_Ext, &envMapAspect_Ext, &entityProperty_Ext, &extEntityPos_Ext,
-	&twoWayPing_Ext, &invOrder_Ext, &fastMap_Ext, &setHotbar_Ext, &setSpawnpoint_Ext, &velControl_Ext,
+	&twoWayPing_Ext, &invOrder_Ext, &instantMOTD_Ext, &fastMap_Ext, &setHotbar_Ext, &setSpawnpoint_Ext, &velControl_Ext,
 	&customParticles_Ext, &customModels_Ext, &pluginMessages_Ext, &extTeleport_Ext,
 #ifdef EXTENDED_TEXTURES
 	&extTextures_Ext,

src/ClassiCube.vcxproj
@@ -469,6 +469,7 @@
     <ClCompile Include="Game.c" />
     <ClCompile Include="Graphics_GL2.c" />
     <ClCompile Include="Graphics_PSP.c" />
+    <ClCompile Include="Graphics_Xbox.c" />
     <ClCompile Include="Gui.c" />
     <ClCompile Include="HeldBlockRenderer.c" />
     <ClCompile Include="Http_Web.c" />
@@ -489,6 +490,7 @@
     <ClCompile Include="Platform_GCWii.c" />
     <ClCompile Include="Platform_Posix.c" />
     <ClCompile Include="Platform_PSP.c" />
+    <ClCompile Include="Platform_PSVita.c" />
     <ClCompile Include="Platform_Web.c" />
     <ClCompile Include="Platform_WinApi.c" />
     <ClCompile Include="Platform_Xbox.c" />

src/ClassiCube.vcxproj.filters
@@ -650,6 +650,12 @@
     <ClCompile Include="Platform_Xbox.c">
       <Filter>Source Files\Platform</Filter>
     </ClCompile>
+    <ClCompile Include="Platform_PSVita.c">
+      <Filter>Source Files\Platform</Filter>
+    </ClCompile>
+    <ClCompile Include="Graphics_Xbox.c">
+      <Filter>Source Files\Graphics</Filter>
+    </ClCompile>
   </ItemGroup>
   <ItemGroup>
     <ResourceCompile Include="..\misc\CCicon.rc">

src/Constants.h
@@ -13,21 +13,6 @@ Copyright 2014-2023 ClassiCube | Licensed under BSD-3
 #define GAME_APP_ALT   "ClassiCube 1.3.5 web mobile"
 #define GAME_APP_NAME  "ClassiCube 1.3.5 web"
 #define GAME_APP_TITLE "ClassiCube"
-#elif defined CC_BUILD_ANDROID
-#define GAME_APP_NAME  "ClassiCube 1.3.5 android alpha"
-#define GAME_APP_TITLE "ClassiCube 1.3.5 android alpha"
-#elif defined CC_BUILD_IOS
-#define GAME_APP_NAME  "ClassiCube 1.3.5 iOS alpha"
-#define GAME_APP_TITLE "ClassiCube 1.3.5 iOS alpha"
-#elif defined CC_BUILD_3DS
-#define GAME_APP_NAME  "ClassiCube 1.3.5 3DS"
-#define GAME_APP_TITLE "ClassiCube 1.3.5 3DS"
-#elif defined CC_BUILD_PSP
-#define GAME_APP_NAME  "ClassiCube 1.3.5 PSP"
-#define GAME_APP_TITLE "ClassiCube 1.3.5 PSP"
-#elif defined CC_BUILD_GCWII
-#define GAME_APP_NAME  "ClassiCube 1.3.5 GC/Wii"
-#define GAME_APP_TITLE "ClassiCube 1.3.5 GC/Wii"
 #else
 #define GAME_APP_NAME  "ClassiCube 1.3.5"
 #define GAME_APP_TITLE "ClassiCube 1.3.5"

src/Platform.h
@@ -36,6 +36,8 @@ extern const cc_result ReturnCode_DirectoryExists;
 /* Whether the launcher and game must both be run in the same process */
 /*  (e.g. can't start a separate process on Mobile or Consoles) */
 extern cc_bool Platform_SingleProcess;
+/* Suffix added to app name sent to the server */
+extern const char* Platform_AppNameSuffix;
 
 #ifdef CC_BUILD_WIN
 typedef struct cc_winstring_ {

src/Platform_3DS.c
@@ -36,6 +36,7 @@ const cc_result ReturnCode_FileNotFound     = ENOENT;
 const cc_result ReturnCode_SocketInProgess  = EINPROGRESS;
 const cc_result ReturnCode_SocketWouldBlock = EWOULDBLOCK;
 const cc_result ReturnCode_DirectoryExists  = EEXIST;
+const char* Platform_AppNameSuffix = " 3DS";
 
 // https://gbatemp.net/threads/homebrew-development.360646/page-245
 // 3DS defaults to stack size of *32 KB*.. way too small

src/Platform_Dreamcast.c
@@ -24,6 +24,7 @@ const cc_result ReturnCode_FileNotFound     = ENOENT;
 const cc_result ReturnCode_SocketInProgess  = EINPROGRESS;
 const cc_result ReturnCode_SocketWouldBlock = EWOULDBLOCK;
 const cc_result ReturnCode_DirectoryExists  = EEXIST;
+const char* Platform_AppNameSuffix = " Dreamcast";
 
 
 /*########################################################################################################################*

src/Platform_GCWii.c
@@ -30,6 +30,11 @@ const cc_result ReturnCode_FileNotFound     = ENOENT;
 const cc_result ReturnCode_SocketInProgess  = -EINPROGRESS; // net_XYZ error results are negative
 const cc_result ReturnCode_SocketWouldBlock = -EWOULDBLOCK;
 const cc_result ReturnCode_DirectoryExists  = EEXIST;
+#ifdef HW_RVL
+const char* Platform_AppNameSuffix = " Wii";
+#else
+const char* Platform_AppNameSuffix = " GameCube";
+#endif
 
 
 /*########################################################################################################################*

commit 4bf1fc4ac1718afae4657e06262cdd34ac2acb4a

Fix b5acd75c3e1dc8cbf9f9dc24388b25e3cff737c1 accidentally causing User Agent header to no longer include special platform names

src/Http_Worker.c
@@ -121,6 +121,17 @@ static void Http_SetRequestHeaders(struct HttpRequest* req) {
 	Http_AddHeader(req, "Cookie", &cookies);
 }
 
+/* TODO: Rewrite to use a local variable instead */
+static cc_string* Http_GetUserAgent_UNSAFE(void) {
+	static char userAgentBuffer[STRING_SIZE];
+	static cc_string userAgent;
+
+	String_InitArray(userAgent, userAgentBuffer);
+	String_AppendConst(&userAgent, GAME_APP_NAME);
+	String_AppendConst(&userAgent, Platform_AppNameSuffix);
+	return &userAgent;
+}
+
 static void Http_SignalWorker(void) { Waitable_Signal(workerWaitable); }
 
 /* Adds a req to the list of pending requests, waking up worker thread if needed */
@@ -634,8 +645,7 @@ static void HttpClientState_Init(struct HttpClientState* state) {
 
 
 static void HttpClient_Serialise(struct HttpClientState* state) {
-	static const cc_string userAgent = String_FromConst(GAME_APP_NAME);
-	static const char* verbs[3] = { "GET", "HEAD", "POST" };
+	static const char* verbs[] = { "GET", "HEAD", "POST" };
 
 	struct HttpRequest* req = state->req;
 	cc_string* buffer = (cc_string*)req->meta;
@@ -645,7 +655,7 @@ static void HttpClient_Serialise(struct HttpClientState* state) {
 					verbs[req->requestType], &state->url.resource);
 
 	Http_AddHeader(req, "Host",       &state->url.address);
-	Http_AddHeader(req, "User-Agent", &userAgent);
+	Http_AddHeader(req, "User-Agent", Http_GetUserAgent_UNSAFE());
 	if (req->data) String_Format1(buffer, "Content-Length: %i\r\n", &req->size);
 
 	Http_SetRequestHeaders(req);
@@ -1306,7 +1316,6 @@ static cc_result Http_SetData(JNIEnv* env, struct HttpRequest* req) {
 }
 
 static cc_result HttpBackend_Do(struct HttpRequest* req, cc_string* url) {
-	static const cc_string userAgent = String_FromConst(GAME_APP_NAME);
 	JNIEnv* env;
 	jint res;
 
@@ -1315,7 +1324,7 @@ static cc_result HttpBackend_Do(struct HttpRequest* req, cc_string* url) {
 	java_req = req;
 
 	Http_SetRequestHeaders(req);
-	Http_AddHeader(req, "User-Agent", &userAgent);
+	Http_AddHeader(req, "User-Agent", Http_GetUserAgent_UNSAFE());
 	if (req->data && (res = Http_SetData(env, req))) return res;
 
 	req->_capacity   = 0;
@@ -1381,7 +1390,6 @@ static cc_result ParseResponseHeaders(struct HttpRequest* req, CFReadStreamRef s
 }
 
 static cc_result HttpBackend_Do(struct HttpRequest* req, cc_string* url) {
-    static const cc_string userAgent = String_FromConst(GAME_APP_NAME);
     static CFStringRef verbs[] = { CFSTR("GET"), CFSTR("HEAD"), CFSTR("POST") };
     cc_bool gotHeaders = false;
     char tmp[NATIVE_STR_LEN];
@@ -1400,7 +1408,7 @@ static cc_result HttpBackend_Do(struct HttpRequest* req, cc_string* url) {
     request = CFHTTPMessageCreateRequest(NULL, verbs[req->requestType], urlRef, kCFHTTPVersion1_1);
     req->meta = request;
     Http_SetRequestHeaders(req);
-    Http_AddHeader(req, "User-Agent", &userAgent);
+    Http_AddHeader(req, "User-Agent", Http_GetUserAgent_UNSAFE());
     CFRelease(urlRef);
     
     if (req->data && req->size) {

src/Program.c
@@ -76,9 +76,9 @@ static int RunProgram(int argc, char** argv) {
 	int argsCount = Platform_GetCommandLineArgs(argc, argv, args);
 #ifdef _MSC_VER
 	/* NOTE: Make sure to comment this out before pushing a commit */
-	//cc_string rawArgs = String_FromConst("UnknownShadow200 fffff 127.0.0.1 25565");
+	cc_string rawArgs = String_FromConst("UnknownShadow200 fffff 127.0.0.1 25565");
 	//cc_string rawArgs = String_FromConst("UnknownShadow200"); 
-	//argsCount = String_UNSAFE_Split(&rawArgs, ' ', args, 4);
+	argsCount = String_UNSAFE_Split(&rawArgs, ' ', args, 4);
 #endif
 
 	if (argsCount == 0) {

src/Protocol.c
@@ -73,7 +73,7 @@ static struct CpeExt
 	blockDefsExt_Ext    = { "BlockDefinitionsExt", 2 },
 	bulkBlockUpdate_Ext = { "BulkBlockUpdate", 1 },
 	textColors_Ext      = { "TextColors", 1 },
-	envMapAspect_Ext    = { "EnvMapAspect", 1 },
+	envMapAspect_Ext    = { "EnvMapAspect", 2 },
 	entityProperty_Ext  = { "EntityProperty", 1 },
 	extEntityPos_Ext    = { "ExtEntityPositions", 1 },
 	twoWayPing_Ext      = { "TwoWayPing", 1 },
@@ -819,7 +819,6 @@ static cc_uint8* Classic_Tick(cc_uint8* data) {
 /*########################################################################################################################*
 *------------------------------------------------------CPE protocol-------------------------------------------------------*
 *#########################################################################################################################*/
-
 static void CPEExtensions_Reset(void) {
 	struct CpeExt* ext;
 	int i;
@@ -842,7 +841,6 @@ static struct CpeExt* CPEExtensions_Find(const cc_string* name) {
 	}
 	return NULL;
 }
-static void CPE_SetMapEnvUrl(cc_uint8* data);
 
 #define Ext_Deg2Packed(x) ((int)((x) * 65536.0f / 360.0f))
 void CPE_SendPlayerClick(int button, cc_bool pressed, cc_uint8 targetId, struct RayTracer* t) {
@@ -1025,6 +1023,14 @@ static void CPE_ExtEntry(cc_uint8* data) {
 #endif
 }
 
+static void CPE_ApplyTexturePack(const cc_string* url) {
+	if (!url->length || Utils_IsUrlPrefix(url)) {
+		Server_RetrieveTexturePack(url);
+	}
+	Platform_Log1("Tex url: %s", url);
+}
+
+
 static void CPE_SetClickDistance(cc_uint8* data) {
 	LocalPlayer_Instance.ReachDistance = Stream_GetU16_BE(data) / 32.0f;
 }
@@ -1173,9 +1179,10 @@ static void CPE_ChangeModel(cc_uint8* data) {
 }
 
 static void CPE_EnvSetMapAppearance(cc_uint8* data) {
+	cc_string url = UNSAFE_GetString(data);
 	int maxViewDist;
+	CPE_ApplyTexturePack(&url);
 
-	CPE_SetMapEnvUrl(data);
 	Env_SetSidesBlock(data[64]);
 	Env_SetEdgeBlock(data[65]);
 	Env_SetEdgeHeight((cc_int16)Stream_GetU16_BE(data + 66));
@@ -1280,11 +1287,7 @@ static void CPE_SetTextColor(cc_uint8* data) {
 
 static void CPE_SetMapEnvUrl(cc_uint8* data) {
 	cc_string url = UNSAFE_GetString(data);
-
-	if (!url.length || Utils_IsUrlPrefix(&url)) {
-		Server_RetrieveTexturePack(&url);
-	}
-	Platform_Log1("Tex url: %s", &url);
+	CPE_ApplyTexturePack(&url);
 }
 
 static void CPE_SetMapEnvProperty(cc_uint8* data) {