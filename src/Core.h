92b81f76b53bdc95248134ecbb3fc1fda1220de2
Jochen Schfer <josch1710@live.de>
2024-04-27 09:16:24 +0200

    Removed OS/2 code from SDL support

--- src/Core.h
+++ src/Core.h
@@ -375,7 +375,7 @@ typedef cc_uint8  cc_bool;
 	#define CC_BUILD_OS2
 	#define CC_BUILD_POSIX
 	#define CC_BUILD_SOFTGPU
-	#define CC_BUILD_SDL2
+	//#define CC_BUILD_SDL2
 	#define CC_BUILD_CURL
 	#define CC_BUILD_FREETYPE
 #elif defined PLAT_SATURN
