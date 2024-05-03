577d69153a329b3eec81366647221998328237a4
UnknownShadow200 <unknownshadow200@gmail.com>
2024-05-03 17:01:38 +1000

    Switch to classicube SDN for updates url

--- src/Constants.h
+++ src/Constants.h
@@ -67,9 +67,8 @@ enum SKIN_TYPE { SKIN_64x32, SKIN_64x64, SKIN_64x64_SLIM, SKIN_INVALID = 0xF0 };
 #define Int32_MinValue  ((cc_int32)-2147483647L - (cc_int32)1L)
 #define Int32_MaxValue  ((cc_int32)2147483647L)
 
-/* Skins were moved to use Amazon S3, so link directly to avoid a pointless redirect */
 #define SKINS_SERVER    "http://cdn.classicube.net/skin"
-#define UPDATES_SERVER  "http://cs.classicube.net/client"
+#define UPDATES_SERVER  "http://cdn.classicube.net/client"
 #define SERVICES_SERVER "https://www.classicube.net/api"
 #define RESOURCE_SERVER "http://static.classicube.net"
 /* Webpage where users can register for a new account */
