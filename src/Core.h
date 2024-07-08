ad2b28cba030508abd44545481115de34c1779e0
UnknownShadow200 <unknownshadow200@gmail.com>
2024-07-09 07:58:39 +1000

    PS2: Actually enable SSL support

--- src/Core.h
+++ src/Core.h
@@ -370,6 +370,7 @@ typedef cc_uint8  cc_bool;
 	#define CC_BUILD_COOPTHREADED
 	#define CC_BUILD_OPENAL
 	#define CC_BUILD_HTTPCLIENT
+	#define CC_BUILD_BEARSSL
 	#define CC_BUILD_SPLITSCREEN
 #elif defined PLAT_NDS
 	#define CC_BUILD_NDS
