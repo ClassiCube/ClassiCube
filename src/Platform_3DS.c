1b48ce3fe3410ef6862d34c7288a69ab201b532d
UnknownShadow200 <unknownshadow200@gmail.com>
2024-08-10 10:08:06 +1000

    Dreamcast: Slightly optimise clipping by avoiding colour interpolation unless necessary

--- src/Platform_3DS.c
+++ src/Platform_3DS.c
@@ -34,9 +34,9 @@
 
 const cc_result ReturnCode_FileShareViolation = 1000000000; /* TODO: not used apparently */
 const cc_result ReturnCode_FileNotFound     = ENOENT;
+const cc_result ReturnCode_DirectoryExists  = EEXIST;
 const cc_result ReturnCode_SocketInProgess  = EINPROGRESS;
 const cc_result ReturnCode_SocketWouldBlock = EWOULDBLOCK;
-const cc_result ReturnCode_DirectoryExists  = EEXIST;
 
 const char* Platform_AppNameSuffix = " 3DS";
 cc_bool Platform_ReadonlyFilesystem;
