b3e545f2023bac6e30d8ed6d55c823b12f18b8b8
UnknownShadow200 <unknownshadow200@gmail.com>
2024-08-16 17:21:29 +1000

    Tidy up Graphics.h a bit

--- src/Graphics.h
+++ src/Graphics.h
@@ -5,7 +5,20 @@
 CC_BEGIN_HEADER
 
 /* 
-Abstracts a 3D graphics rendering API
+SUMMARY:
+- Provides a low level abstraction a 3D graphics rendering API.
+- Because of the numerous possible rendering backends, only a small number of
+   functions are provided so that the available functionality behaves the same
+   regardless of the rendering backend being used. (as much as reasonably possible)
+- Most code using Graphics.h therefore doesn' need to care about the rendering backend being used
+
+IMPLEMENTATION NOTES:
+- By default, a reasonable rendering backend is automatically selected in Core.h
+- The selected rendering backend can be altered in two ways:
+  * explicitly defining CC_GFX_BACKEND in the compilation flags (recommended)
+  * altering DEFAULT_GFX_BACKEND for the platform in Core.h
+- Rendering backends are implemented in Graphics_GL1.c, Graphics_D3D9.c etc
+   
 Copyright 2014-2023 ClassiCube | Licensed under BSD-3
 */
 struct Bitmap;
@@ -17,15 +30,6 @@ extern struct IGameComponent Gfx_Component;
 typedef enum VertexFormat_ {
 	VERTEX_FORMAT_COLOURED, VERTEX_FORMAT_TEXTURED
 } VertexFormat;
-
-typedef enum FogFunc_ {
-	FOG_LINEAR, FOG_EXP, FOG_EXP2
-} FogFunc;
-
-typedef enum MatrixType_ {
-	MATRIX_PROJ, /* Projection matrix */
-	MATRIX_VIEW  /* Combined model view matrix */
-} MatrixType;
 
 #define SIZEOF_VERTEX_COLOURED 16
 #define SIZEOF_VERTEX_TEXTURED 24
@@ -88,11 +92,13 @@ extern const cc_string Gfx_LowPerfMessage;
 #define GFX_MAX_INDICES (65536 / 4 * 6)
 #define GFX_MAX_VERTICES 65536
 
-typedef enum GfxBuffers_ {
-	GFX_BUFFER_COLOR = 1,
-	GFX_BUFFER_DEPTH = 2
-} GfxBuffers;
+void  Gfx_RecreateTexture(GfxResourceID* tex, struct Bitmap* bmp, cc_uint8 flags, cc_bool mipmaps);
+void* Gfx_RecreateAndLockVb(GfxResourceID* vb, VertexFormat fmt, int count);
 
+
+/*########################################################################################################################*
+*---------------------------------------------------------Textures--------------------------------------------------------*
+*#########################################################################################################################*/
 /* Texture should persist across gfx context loss (if backend supports ManagedTextures) */
 #define TEXTURE_FLAG_MANAGED  0x01
 /* Texture should allow updating via Gfx_UpdateTexture */
@@ -103,9 +109,6 @@ typedef enum GfxBuffers_ {
 #define TEXTURE_FLAG_LOWRES   0x08
 /* Texture should be rendered using bilinear filtering if possible */
 #define TEXTURE_FLAG_BILINEAR 0x10
-
-void  Gfx_RecreateTexture(GfxResourceID* tex, struct Bitmap* bmp, cc_uint8 flags, cc_bool mipmaps);
-void* Gfx_RecreateAndLockVb(GfxResourceID* vb, VertexFormat fmt, int count);
 
 cc_bool Gfx_CheckTextureSize(int width, int height, cc_uint8 flags);
 /* Creates a new texture. (and also generates mipmaps if mipmaps) */
@@ -134,6 +137,46 @@ CC_API void Gfx_EnableMipmaps(void);
 /* NOTE: You must have created textures with mipmaps true for this to work */
 CC_API void Gfx_DisableMipmaps(void);
 
+
+/*########################################################################################################################*
+*------------------------------------------------------Frame management---------------------------------------------------*
+*#########################################################################################################################*/
+typedef enum GfxBuffers_ {
+	GFX_BUFFER_COLOR = 1,
+	GFX_BUFFER_DEPTH = 2
+} GfxBuffers;
+
+/* Clears the given rendering buffer(s) to their default values. */
+/* buffers can be either GFX_BUFFER_COLOR or GFX_BUFFER_DEPTH, or both */
+CC_API void Gfx_ClearBuffers(GfxBuffers buffers);
+/* Sets the default colour that the colour buffer is cleared to */
+CC_API void Gfx_ClearColor(PackedCol color);
+
+/* Sets up state for rendering a new frame */
+void Gfx_BeginFrame(void);
+/* Finishes rendering a frame, and swaps it with the back buffer */
+void Gfx_EndFrame(void);
+/* Sets whether to synchronise with monitor refresh to avoid tearing */
+/* NOTE: VSync setting may be unsupported or just ignored */
+void Gfx_SetVSync(cc_bool vsync);
+
+enum Screen3DS { TOP_SCREEN, BOTTOM_SCREEN };
+#ifdef CC_BUILD_DUALSCREEN
+/* Selects which screen/display to render to */
+void Gfx_3DS_SetRenderScreen(enum Screen3DS screen);
+#else
+/* Selects which screen/display to render to */
+static CC_INLINE void Gfx_3DS_SetRenderScreen(enum Screen3DS screen) { }
+#endif
+
+
+/*########################################################################################################################*
+*---------------------------------------------------------Fog state-------------------------------------------------------*
+*#########################################################################################################################*/
+typedef enum FogFunc_ {
+	FOG_LINEAR, FOG_EXP, FOG_EXP2
+} FogFunc;
+
 /* Returns whether fog blending is enabled */
 CC_API cc_bool Gfx_GetFog(void);
 /* Sets whether fog blending is enabled */
@@ -147,6 +190,10 @@ CC_API void Gfx_SetFogEnd(float value);
 /* Sets in what way fog is blended */
 CC_API void Gfx_SetFogMode(FogFunc func);
 
+
+/*########################################################################################################################*
+*-----------------------------------------------------State management----------------------------------------------------*
+*#########################################################################################################################*/
 /* Sets whether backface culling is performed */
 CC_API void Gfx_SetFaceCulling(cc_bool enabled);
 /* Sets whether pixels with an alpha of less than 128 are discarded */
@@ -156,11 +203,6 @@ CC_API void Gfx_SetAlphaBlending(cc_bool enabled);
 /* Sets whether blending between the alpha components of texture and vertex colour is performed */
 CC_API void Gfx_SetAlphaArgBlend(cc_bool enabled);
 
-/* Clears the given rendering buffer(s) to default. */
-/* buffers can be either GFX_BUFFER_COLOR or GFX_BUFFER_DEPTH, or both */
-CC_API void Gfx_ClearBuffers(GfxBuffers buffers);
-/* Sets the colour that the colour buffer is cleared to */
-CC_API void Gfx_ClearColor(PackedCol color);
 /* Sets whether pixels may be discard based on z/depth */
 CC_API void Gfx_SetDepthTest(cc_bool enabled);
 /* Sets whether z/depth of pixels is actually written to the depth buffer */
@@ -171,11 +213,10 @@ CC_API void Gfx_SetColorWrite(cc_bool r, cc_bool g, cc_bool b, cc_bool a);
 /*  NOTE: Implicitly calls Gfx_SetColorWrite */
 CC_API void Gfx_DepthOnlyRendering(cc_bool depthOnly);
 
-/* Anaglyph 3D rendering support */
-void Gfx_Set3DLeft( struct Matrix* proj, struct Matrix* view);
-void Gfx_Set3DRight(struct Matrix* proj, struct Matrix* view);
-void Gfx_End3D(     struct Matrix* proj, struct Matrix* view);
 
+/*########################################################################################################################*
+*------------------------------------------------------Index buffers-----------------------------------------------------*
+*#########################################################################################################################*/
 /* Callback function to initialise/fill out the contents of an index buffer */
 typedef void (*Gfx_FillIBFunc)(cc_uint16* indices, int count, void* obj);
 /* Creates a new index buffer and fills out its contents */
@@ -185,6 +226,10 @@ CC_API void Gfx_BindIb(GfxResourceID ib);
 /* Deletes the given index buffer, then sets it to 0 */
 CC_API void Gfx_DeleteIb(GfxResourceID* ib);
 
+
+/*########################################################################################################################*
+*------------------------------------------------------Vertex buffers-----------------------------------------------------*
+*#########################################################################################################################*/
 /* Creates a new vertex buffer */
 CC_API GfxResourceID Gfx_CreateVb(VertexFormat fmt, int count);
 /* Sets the currently active vertex buffer */
@@ -222,6 +267,10 @@ CC_API void  Gfx_UnlockDynamicVb(GfxResourceID vb);
 /* Updates the data of a dynamic vertex buffer */
 CC_API void Gfx_SetDynamicVbData(GfxResourceID vb, void* vertices, int vCount);
 
+
+/*########################################################################################################################*
+*------------------------------------------------------Vertex drawing-----------------------------------------------------*
+*#########################################################################################################################*/
 /* Sets the format of the rendered vertices */
 CC_API void Gfx_SetVertexFormat(VertexFormat fmt);
 /* Renders vertices from the currently bound vertex buffer as lines */
@@ -234,9 +283,20 @@ CC_API void Gfx_DrawVb_IndexedTris(int verticesCount);
 /* Special case Gfx_DrawVb_IndexedTris_Range for map renderer */
 void Gfx_DrawIndexedTris_T2fC4b(int verticesCount, int startVertex);
 
-/* Loads the given matrix over the currently active matrix */
+
+/*########################################################################################################################*
+*-----------------------------------------------------Vertex transform----------------------------------------------------*
+*#########################################################################################################################*/
+typedef enum MatrixType_ {
+	MATRIX_PROJ, /* Projection matrix */
+	MATRIX_VIEW  /* Combined model view matrix */
+} MatrixType;
+
+/* Sets the currently active matrix projection or modelview matrix */
 CC_API void Gfx_LoadMatrix(MatrixType type, const struct Matrix* matrix);
+/* Sets the texture U/V translation (not normally used) */
 CC_API void Gfx_EnableTextureOffset(float x, float y);
+/* Disables texture U/V translation */
 CC_API void Gfx_DisableTextureOffset(void);
 /* Loads given modelview and projection matrices, then calculates the combined MVP matrix */
 void Gfx_LoadMVP(const struct Matrix* view, const struct Matrix* proj, struct Matrix* mvp);
@@ -248,19 +308,25 @@ void Gfx_CalcPerspectiveMatrix(struct Matrix* matrix, float fov, float aspect, f
 /* NOTE: Projection matrix calculation is here because it can depend the graphics backend */
 /*  (e.g. OpenGL uses a Z clip space range of [-1, 1], whereas Direct3D9 uses [0, 1]) */
 
+/* Sets the region where transformed vertices are drawn in */
+/*  By default this region has origin 0,0 and size is window width/height */
+/*  This region should normally be the same as the scissor region */
+CC_API void Gfx_SetViewport(int x, int y, int w, int h);
+/* Sets the region where pixels can be drawn in (pixels outside this region are discarded) */
+/*  By default this region has origin 0,0 and size is window width/height */
+/*  This region should normally be the same as the viewport region */
+CC_API void Gfx_SetScissor (int x, int y, int w, int h);
+
+
+/*########################################################################################################################*
+*------------------------------------------------------Misc utilities-----------------------------------------------------*
+*#########################################################################################################################*/
 /* Outputs a .png screenshot of the backbuffer */
 cc_result Gfx_TakeScreenshot(struct Stream* output);
 /* Warns in chat if the backend has problems with the user's GPU */
 /* Returns whether legacy rendering mode for borders/sky/clouds is needed */
 cc_bool Gfx_WarnIfNecessary(void);
 cc_bool Gfx_GetUIOptions(struct MenuOptionsScreen* s);
-/* Sets up state for rendering a new frame */
-void Gfx_BeginFrame(void);
-/* Finishes rendering a frame, and swaps it with the back buffer */
-void Gfx_EndFrame(void);
-/* Sets whether to synchronise with monitor refresh to avoid tearing */
-/* NOTE: VSync setting may be unsupported or just ignored */
-void Gfx_SetVSync(cc_bool vsync);
 /* Gets information about the user's GPU and current backend state */
 /* Backend state may include depth buffer bits, free memory, etc */
 /* NOTE: Each line is separated by \n */
@@ -269,16 +335,11 @@ void Gfx_GetApiInfo(cc_string* info);
 /* Updates state when the window's dimensions have changed */
 /* NOTE: This may require recreating the context depending on the backend */
 void Gfx_OnWindowResize(void);
-CC_API void Gfx_SetViewport(int x, int y, int w, int h);
-CC_API void Gfx_SetScissor (int x, int y, int w, int h);
 
-enum Screen3DS { TOP_SCREEN, BOTTOM_SCREEN };
-#ifdef CC_BUILD_DUALSCREEN
-/* Selects which screen on the 3DS to render to */
-void Gfx_3DS_SetRenderScreen(enum Screen3DS screen);
-#else
-static CC_INLINE void Gfx_3DS_SetRenderScreen(enum Screen3DS screen) { }
-#endif
+/* Anaglyph 3D rendering support */
+void Gfx_Set3DLeft( struct Matrix* proj, struct Matrix* view);
+void Gfx_Set3DRight(struct Matrix* proj, struct Matrix* view);
+void Gfx_End3D(     struct Matrix* proj, struct Matrix* view);
 
 /* Raises ContextLost event and updates state for lost contexts */
 void Gfx_LoseContext(const char* reason);
@@ -287,6 +348,15 @@ void Gfx_RecreateContext(void);
 /* Attempts to restore a lost context */
 cc_bool Gfx_TryRestoreContext(void);
 
+/* Sets appropriate alpha test/blending for given block draw type */
+void Gfx_SetupAlphaState(cc_uint8 draw);
+/* Undoes changes to alpha test/blending state by Gfx_SetupAlphaState */
+void Gfx_RestoreAlphaState(cc_uint8 draw);
+
+
+/*########################################################################################################################*
+*------------------------------------------------------2D rendering------------------------------------------------------*
+*#########################################################################################################################*/
 /* Renders a 2D flat coloured rectangle */
 void Gfx_Draw2DFlat(int x, int y, int width, int height, PackedCol color);
 /* Renders a 2D flat vertical gradient rectangle */
@@ -302,11 +372,6 @@ void Gfx_Begin2D(int width, int height);
 /* Switches state to be suitable for drawing 3D graphics */
 /* NOTE: This means restoring fog/depth test, restoring matrices, etc */
 void Gfx_End2D(void);
-
-/* Sets appropriate alpha test/blending for given block draw type */
-void Gfx_SetupAlphaState(cc_uint8 draw);
-/* Undoes changes to alpha test/blending state by Gfx_SetupAlphaState */
-void Gfx_RestoreAlphaState(cc_uint8 draw);
 
 /* Statically initialises the position and dimensions of this texture */
 #define Tex_Rect(x,y, width,height) x,y,width,height
