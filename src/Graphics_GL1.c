72ea3e3be83a22f88ba7f21e833dc3c905e85401
UnknownShadow200 <unknownshadow200@gmail.com>
2025-05-25 08:02:10 +1000

    More workflows treat warnings as errors

--- src/Graphics_GL1.c
+++ src/Graphics_GL1.c
@@ -480,7 +480,9 @@ static void Gfx_RestoreState(void) {
 	gfx_format = -1;
 
 	_glHint(GL_FOG_HINT, GL_NICEST);
+#if defined CC_BUILD_SYMBIAN
 	_glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+#endif
 
 	_glAlphaFunc(GL_GREATER, 0.5f);
 	_glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
