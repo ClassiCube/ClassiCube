8a8df0bfa429455831f9923ad1b613381b0b89dd
UnknownShadow200 <unknownshadow200@gmail.com>
2025-08-31 07:50:26 +1000

    Convert unicode to code page 437 for JSON output, partially addresses #1428

--- src/LWeb.c
+++ src/LWeb.c
@@ -99,8 +99,7 @@ static void Json_ConsumeString(struct JsonContext* ctx, cc_string* str) {
 
 		codepoint = (h[0] << 12) | (h[1] << 8) | (h[2] << 4) | h[3];
 		/* don't want control characters in names/software */
-		/* TODO: Convert to CP437.. */
-		if (codepoint >= 32) String_Append(str, codepoint);
+		if (codepoint >= 32) String_Append(str, Convert_CodepointToCP437(codepoint));
 		JsonContext_Consume(ctx, 4);
 	}
 
