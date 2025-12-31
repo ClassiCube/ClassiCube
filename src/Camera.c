9cca0c97c8dfdc3cbb0c6aa724c2497025a0927d
impresson <kitnap1230@gmail.com>
2025-12-31 21:28:47 +1300

    Updated NoCamGravity

--- src/Camera.c
+++ src/Camera.c
@@ -171,8 +171,8 @@ static void PerspectiveCamera_CalcViewBobbing(struct LocalPlayer* p, float t, fl
 
 	/* When standing on the ground, velocity.y is -0.08 (-gravity) */
 	/* So add 0.08 to counteract that, so that vel is 0 when standing on ground */
-	vel  = gravity + Math_Lerp(p->OldVelocity.y, e->Velocity.y, t);
-	if (hacks->Flying ) vel = 0.0f;
+	if (!p->Base.OnGround) vel = gravity + Math_Lerp(p->OldVelocity.y, e->Velocity.y, t);
+	if (!NoCamGravity_enabled) vel = 0.0f; 
 	if (!NoCamGravity_enabled) {
 		fall = -vel * 0.05f * velTiltStrength / velTiltScale;
 	if (fall > 0.5f) {
