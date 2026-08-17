70af783ac9af36c7a44a47df7a95d6c84339c7cc
UnknownShadow200 <unknownshadow200@gmail.com>
2026-08-17 20:02:59 +1000

    Makefile: try to make it easier to build windows binaries with icon

--- misc/makefiles/windows.mk
+++ misc/makefiles/windows.mk
@@ -11,6 +11,26 @@ default: $(TARGET).exe
 
 
 #-----------------------------
+# windres auto detection
+#-----------------------------
+# tricky because windres is arch and toolchain dependent, but still try to auto guess
+# TODO maybe just substitute in path.. ? check windows behaviour
+ifneq ($(strip $(WINDRES)),)
+    # already set
+else ifeq ($(CC),x86_64-w64-mingw32-gcc)
+    WINDRES := x86_64-w64-mingw32-windres
+else ifeq ($(CC),i686-w64-mingw32-gcc)
+    WINDRES := i686-w64-mingw32-windres
+else ifeq ($(CC),armv7-w64-mingw32-gcc)
+    WINDRES := armv7-w64-mingw32-windres
+else ifeq ($(CC),aarch64-w64-mingw32)
+    WINDRES := aarch64-w64-mingw32-windres
+else
+    $(warning WINDRES not set. Generated $(TARGET).exe will not have an icon.)
+endif
+
+
+#-----------------------------
 # Icon generation
 #-----------------------------
 ifneq ($(strip $(WINDRES)),)
